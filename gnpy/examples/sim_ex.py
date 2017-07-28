2ec50c4e07a8395a565fa6efef77cecbd2de9ff6
Gilad Goldfarb <giladg@fb.com>
2017-07-28 15:29:29 -0700

    with NW graph plots

--- gnpy/examples/sim_ex.py
+++ gnpy/examples/sim_ex.py
@@ -21,37 +21,16 @@ if __name__ == '__main__':
                 for x in channels)
             print(f'{u.id:^10s} → {v.id:^10s} {channel_info}')
 
-exit()
-
-
-config_fn = './gnpy/examples/config/config_ex1.json'
-nw = gnpy.Network(config_fn)
-nw.propagate_all_paths()
-
-# output OSNR propagation
-edge_fmt = "ν: {frequency}\nOSNR:{osnr:^7.2f}\npow:{power:^8.1f}"
-elabels = {}
-for path in nw.tr_paths:
-    print("Path", path.path)
-    print("{:^8}{:^11}{:^9}{:^5}{:^11}".format("n0", "n1", "ν", "OSNR", "pow"))
-    for edge in path.edge_list:
-        spectrum = nw.g[edge[0]][edge[1]]['channels']
-        pspec = [edge_fmt.format(**s) for s in spectrum]
-        elabels[edge] = '\n'.join(pspec)
-        print("{:^8}..{:^8}" .format(str(edge[0]), str(edge[1])),
-              pspec[0].replace('\n', ' ').replace('OSNR', '')
-              .replace('pow', "").replace("ν", "").replace(":", ""))
-
-if 0:
-    import networkx as nx
-    layout = nx.spring_layout(nw.g)
-    nx.draw_networkx_nodes(nw.g, layout, node_size=1000,
-                           node_color='b', alpha=0.2, node_shape='s')
-    nx.draw_networkx_labels(nw.g, layout)
-    nx.draw_networkx_edges(nw.g, layout, width=2,
-                           alpha=0.3, edge_color='green')
-    nx.draw_networkx_edge_labels(
-        nw.g, layout, edge_labels=elabels, font_size=10)
-    plt.rcdefaults()
-    plt.axis('off')
-    plt.show()
+    if 1:  # plot network graph
+        import networkx as nx
+        import matplotlib.pyplot as plt
+        layout = nx.spring_layout(nw.g)
+        nx.draw_networkx_nodes(nw.g, layout, node_size=1000,
+                               node_color='b', alpha=0.2, node_shape='s')
+        nx.draw_networkx_labels(nw.g, layout)
+        nx.draw_networkx_edges(nw.g, layout, width=2,
+                               alpha=0.3, edge_color='green')
+        nx.draw_networkx_edge_labels(nw.g, layout, font_size=10)
+        plt.rcdefaults()
+        plt.axis('off')
+        plt.show()
