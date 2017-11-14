ef6a8ba5a6652f580376c6ff5aa38b70390d3c2e
Brian Taylor <briantaylor@fb.com>
2017-11-14 14:33:44 -0800

    adding rrc filter, temporarily putting it in utilities.py

--- utilities.py
+++ utilities.py
@@ -5,6 +5,8 @@ Created on Fri Nov 10 17:50:46 2017
 
 @author: briantaylor
 """
+import numpy as np
+from numpy import pi, cos, sqrt
 
 def c():
     """
@@ -39,3 +41,30 @@ def deltaf2deltawl(delta_f, frequency):
     '''
     wl = freq2wavelength(frequency)
     return delta_f*wl/frequency
+
+
+def rrc(ffs, baud_rate, alpha):
+    """ rrc(ffs, baud_rate, alpha): computes the root-raised cosine filter 
+    function.
+    
+    :param ffs: A numpy array of frequencies
+    :param baud_rate: The Baud Rate of the System
+    :param alpha: The roll-off factor of the filter
+    :type ffs: numpy.ndarray
+    :type baud_rate: float
+    :type alpha: float
+    :return: hf a numpy array of the filter shape
+    :rtype: numpy.ndarray
+    
+    """
+    Ts = 1/baud_rate
+    l_lim = (1 - alpha)/(2 * Ts)
+    r_lim = (1 + alpha)/(2 * Ts)
+    hf = np.zeros(np.shape(ffs))
+    slope_inds = np.where(
+            np.logical_and(np.abs(ffs) > l_lim, np.abs(ffs) < r_lim))
+    hf[slope_inds] = 0.5 * (1 + cos((pi * Ts / alpha) * 
+            (np.abs(ffs[slope_inds]) - l_lim)))
+    p_inds = np.where(np.logical_and(np.abs(ffs) > 0, np.abs(ffs) < l_lim))
+    hf[p_inds] =  1
+    return sqrt(hf)
