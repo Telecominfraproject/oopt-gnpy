5646714c133fd6682177fe52986c7055422658fc
James Powell <james@dontusethiscode.com>
2018-03-16 16:45:18 -0400

    small setup.py fixes

--- setup.py
+++ setup.py
@@ -11,29 +11,26 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='gnpy',
-    version='0.0.1',
-    description='TIP optical network modeling library',
+    version='0.1.0',
+    description='route planning and optimization tool for mesh optical networks',
     long_description=long_description,
     url='https://github.com/Telecominfraproject/gnpy',
     author='Telecom Infra Project',
     classifiers=[
         'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Telecommunications Industry',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
         'Topic :: Scientific/Engineering',
         'Topic :: Scientific/Engineering :: Physics',
-        'License :: OSI Approved :: BSD License',
-        'Programming Language :: Python :: 3.6',
     ],
-    keywords='optics network fiber communication',
+    keywords='optics network fiber communication route planning optimization',
     packages=find_packages(exclude=['examples', 'docs', 'tests']),  # Required
-    install_requires=['cycler',
-                      'decorator',
-                      'matplotlib',
-                      'networkx',
-                      'numpy',
-                      'scipy',
-                      'pyparsing',
-                      'python-dateutil',
-                      'pytz',
-                      'six',
-                      'xlrd']
+    install_requires=list(open('requirements.txt'))
 )
