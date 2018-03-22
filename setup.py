2dd096cfab529f33d6d132795c7706d2bed92f68
James Powell <james@dontusethiscode.com>
2018-03-22 13:07:58 -0400

    bump version to v0.1.2

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='gnpy',
-    version='0.1.1',
+    version='0.1.2',
     description='route planning and optimization tool for mesh optical networks',
     long_description=long_description,
     long_description_content_type='text/x-rst',
