2d515eea4ca1dc877a1f11a967babb68a96839a6
EstherLerouzic <esther.lerouzic@orange.com>
2018-12-13 11:28:42 +0000

    Corrections of some bugs
    - output file of -o option in path_requests_run.py was not correctly
      handled in case of path indirection eg ../../bar.foo
    - fiber constraint is not correctly handled in case of several parrallel directions
      of same fiber name. remove if from the set of constraint till problem is not solved
    - loose_list was not correctly indexed in request.py. assume the first element attribute (except
      of the transceiver) applies for the whole list. This will need further corrections
    - handle the case when path.snr is none in the optimization of mode process

--- gnpy/core/request.py
+++ gnpy/core/request.py
@@ -307,7 +307,7 @@ def compute_constrained_path(network, req):
             candidate.sort(key=lambda x: len(x))
             total_path = candidate[0]
         else:
-            if req.loose_list[req.nodes_list.index(n)] == 'loose':
+            if req.loose_list[1] == 'loose':
                 print(f'Request {req.request_id} could not find a path crossing {nodes_list} in network topology')
                 print(f'constraint ignored')
                 total_path = dijkstra_path(network, source, destination)
@@ -421,9 +421,12 @@ def propagate_and_optimize_mode(path, req, equipment, show=False):
                 if show :
                     print(el)
             for m in modes_to_explore :
-                if round(mean(path[-1].snr+lin2db(b/(12.5e9))),2) > m['OSNR'] :
-                    found_a_feasible_mode = True
-                    return path, m
+                if path[-1].snr is not None:
+                    if round(mean(path[-1].snr+lin2db(b/(12.5e9))),2) > m['OSNR'] :
+                        found_a_feasible_mode = True
+                        return path, m
+                else:  
+                    return [], None
         # only get to this point if no budrate/mode staisfies OSNR requirement
         # returns the last propagated path and mode
         msg = f'Warning! Request {req.request_id}: no mode satisfies path SNR requirement.\n'
