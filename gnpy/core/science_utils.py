471eab126ea8a1fa9fdd2145528a1c84465c5530
Alessio Ferrari <alessio.ferrari@polito.it>
2019-06-11 13:38:05 +0200

    fix raman solver parameters

--- gnpy/core/science_utils.py
+++ gnpy/core/science_utils.py
@@ -1,15 +1,13 @@
-import progressbar
 import numpy as np
 from operator import attrgetter
-from scipy.interpolate import interp1d
-
-from gnpy.core.utils import load_json
+from collections import namedtuple
 import scipy.constants as ph
 from scipy.integrate import solve_bvp
 from scipy.integrate import cumtrapz
 from scipy.interpolate import interp1d
 from scipy.optimize import OptimizeResult
-from gnpy.core.elements import RamanFiber
+
+from gnpy.core.utils import db2lin, load_json
 
 
 def load_sim_params(path_sim_params):
@@ -17,6 +15,7 @@ def load_sim_params(path_sim_params):
     return SimParams(params=sim_params)
 
 def configure_network(network, sim_params):
+    from gnpy.core.elements import RamanFiber
     for node in network.nodes:
         if isinstance(node, RamanFiber):
             node.sim_params = sim_params
@@ -46,104 +45,102 @@ class SimParams():
             self.nli_params = NLIParams(params=params['nli_parameters'])
 
 class RamanSolver:
-    def __init__(self, fiber_information=None):
+    def __init__(self, raman_params=None, fiber_params=None):
         """ Initialize the fiber object with its physical parameters
         :param length: fiber length in m.
         :param alphap: fiber power attenuation coefficient vs frequency in 1/m. numpy array
         :param freq_alpha: frequency axis of alphap in Hz. numpy array
         :param cr_raman: Raman efficiency vs frequency offset in 1/W/m. numpy array
         :param freq_cr: reference frequency offset axis for cr_raman. numpy array
-        :param solver_params: namedtuple containing the solver parameters (optional).
+        :param raman_params: namedtuple containing the solver parameters (optional).
         """
-        self._fiber_information = fiber_information
-        self._solver_params = None
-        self._spectral_information = None
-        self._raman_pump_information = None
-        self._stimulated_raman_scattering = None
-        self._spontaneous_raman_scattering = None
+        self.fiber_params = fiber_params
+        self.raman_params = raman_params
+        self.__carriers = None
+        self.__stimulated_raman_scattering = None
+        self.__spontaneous_raman_scattering = None
 
     @property
-    def fiber_information(self):
-        return self._fiber_information
+    def fiber_params(self):
+        return self.__fiber_params
 
-    @fiber_information.setter
-    def fiber_information(self, fiber_information):
-        self._fiber_information = fiber_information
-        self._stimulated_raman_scattering = None
+    @fiber_params.setter
+    def fiber_params(self, fiber_params):
+        self.__stimulated_raman_scattering = None
+        self.__fiber_params = fiber_params
 
     @property
-    def spectral_information(self):
-        return self._spectral_information
+    def carriers(self):
+        return self.__carriers
 
-    @spectral_information.setter
-    def spectral_information(self, spectral_information):
+    @carriers.setter
+    def carriers(self, carriers):
         """
-        :param spectral_information: namedtuple containing all the spectral information about carriers and eventual Raman pumps
+        :param carriers: tuple of namedtuples containing information about carriers
         :return:
         """
-        self._spectral_information = spectral_information
-        self._stimulated_raman_scattering = None
+        self.__carriers = carriers
+        self.__stimulated_raman_scattering = None
 
     @property
-    def raman_pump_information(self):
-        return self._raman_pump_information
+    def raman_pumps(self):
+        return self._raman_pumps
 
-    @raman_pump_information.setter
-    def raman_pump_information(self, raman_pump_information):
-        self._raman_pump_information = raman_pump_information
+    @raman_pumps.setter
+    def raman_pumps(self, raman_pumps):
+        self._raman_pumps = raman_pumps
+        self.__stimulated_raman_scattering = None
 
     @property
-    def solver_params(self):
-        return self._solver_params
+    def raman_params(self):
+        return self.__raman_params
 
-    @solver_params.setter
-    def solver_params(self, solver_params):
+    @raman_params.setter
+    def raman_params(self, raman_params):
         """
-        :param solver_params: namedtuple containing the solver parameters (optional).
+        :param raman_params: namedtuple containing the solver parameters (optional).
         :return:
         """
-        self._solver_params = solver_params
-        self._stimulated_raman_scattering = None
-        self._spontaneous_raman_scattering = None
+        self.__raman_params = raman_params
+        self.__stimulated_raman_scattering = None
+        self.__spontaneous_raman_scattering = None
 
     @property
     def spontaneous_raman_scattering(self):
-        if self._spontaneous_raman_scattering is None:
+        if self.__spontaneous_raman_scattering is None:
             # SET STUFF
-            attenuation_coefficient = self.fiber_information.attenuation_coefficient
-            raman_coefficient = self.fiber_information.raman_coefficient
-            temp_k = self.fiber_information.temperature
+            loss_coef = self.fiber_params.loss_coef
+            raman_efficiency = self.fiber_params.raman_efficiency
+            temperature = self.fiber_params.temperature
 
-            spectral_info = self.spectral_information
-            raman_pump_information = self.raman_pump_information
+            carriers = self.carriers
+            raman_pumps = self.raman_pumps
 
-            verbose = self.solver_params.verbose
+            verbose = self.raman_params.verbose
 
             if verbose:
                 print('Start computing fiber Spontaneous Raman Scattering')
 
-            power_spectrum, freq_array, prop_direct, bn_array = self._compute_power_spectrum(spectral_info, raman_pump_information)
-            temperature = temp_k.temperature
+            power_spectrum, freq_array, prop_direct, bn_array = self._compute_power_spectrum(carriers, raman_pumps)
 
-            if len(attenuation_coefficient.alpha_power) >= 2:
-                interp_alphap = interp1d(attenuation_coefficient.frequency, attenuation_coefficient.alpha_power)
-                alphap_fiber = interp_alphap(freq_array)
+            if not hasattr(loss_coef, 'alpha_power'):
+                alphap_fiber = loss_coef * np.ones(freq_array.shape)
             else:
-                alphap_fiber = attenuation_coefficient.alpha_power * np.ones(freq_array.shape)
+                interp_alphap = interp1d(loss_coef['frequency'], loss_coef['alpha_power'])
+                alphap_fiber = interp_alphap(freq_array)
 
             freq_diff = abs(freq_array - np.reshape(freq_array, (len(freq_array), 1)))
-            if len(raman_coefficient.cr) >= 2:
-                interp_cr = interp1d(raman_coefficient.frequency, raman_coefficient.cr)
-                cr = interp_cr(freq_diff)
-            else:
-                cr = raman_coefficient.cr * np.ones(freq_diff.shape)
+            interp_cr = interp1d(raman_efficiency['frequency_offset'], raman_efficiency['cr'])
+            cr = interp_cr(freq_diff)
 
             # z propagation axis
-            z_array = self.stimulated_raman_scattering.z
+            z_array = self.__stimulated_raman_scattering.z
             ase_bc = np.zeros(freq_array.shape)
 
             # calculate ase power
-            spontaneous_raman_scattering = self._int_spontaneous_raman(z_array, self.stimulated_raman_scattering.power, alphap_fiber, freq_array, cr, freq_diff, ase_bc, bn_array, temperature)
+            spontaneous_raman_scattering = self._int_spontaneous_raman(z_array, self.__stimulated_raman_scattering.power,
+                                                                       alphap_fiber, freq_array, cr, freq_diff, ase_bc,
+                                                                       bn_array, temperature)
 
             setattr(spontaneous_raman_scattering, 'frequency', freq_array)
             setattr(spontaneous_raman_scattering, 'z', z_array)
@@ -153,16 +150,16 @@ class RamanSolver:
             if verbose:
                 print(spontaneous_raman_scattering.message)
 
-            self._spontaneous_raman_scattering = spontaneous_raman_scattering
+            self.__spontaneous_raman_scattering = spontaneous_raman_scattering
 
-        return self._spontaneous_raman_scattering
+        return self.__spontaneous_raman_scattering
 
     @staticmethod
-    def _compute_power_spectrum(spectral_information, raman_pump_information):
+    def _compute_power_spectrum(carriers, raman_pumps=None):
         """
         Rearrangement of spectral and Raman pump information to make them compatible with Raman solver
-        :param spectral_information: a namedtuple describing the transmitted channels
-        :param raman_pump_information: a namedtuple describing the Raman pumps
+        :param carriers: a tuple of namedtuples describing the transmitted channels
+        :param raman_pumps: a namedtuple describing the Raman pumps
         :return:
         """
 
@@ -170,20 +167,22 @@ class RamanSolver:
         pow_array = np.array([])
         f_array = np.array([])
         noise_bandwidth_array = np.array([])
-        for carrier in sorted(spectral_information.carriers, key=attrgetter('frequency')):
+        for carrier in sorted(carriers, key=attrgetter('frequency')):
             f_array = np.append(f_array, carrier.frequency)
             pow_array = np.append(pow_array, carrier.power.signal)
-            noise_bandwidth_array = np.append(noise_bandwidth_array, carrier.baud_rate)
+            ref_bw = carrier.baud_rate
+            noise_bandwidth_array = np.append(noise_bandwidth_array, ref_bw)
 
         propagation_direction = np.ones(len(f_array))
 
         # Raman pump power spectrum
-        if raman_pump_information:
-            for pump in raman_pump_information.raman_pumps:
+        if raman_pumps:
+            for pump in raman_pumps:
                 pow_array = np.append(pow_array, pump.power)
                 f_array = np.append(f_array, pump.frequency)
-                propagation_direction = np.append(propagation_direction, pump.propagation_direction)
-                noise_bandwidth_array = np.append(noise_bandwidth_array, pump.pump_bandwidth)
+                direction = +1 if pump.propagation_direction.lower() == 'coprop' else -1
+                propagation_direction = np.append(propagation_direction, direction)
+                noise_bandwidth_array = np.append(noise_bandwidth_array, ref_bw)
 
         # Final sorting
         ind = np.argsort(f_array)
@@ -196,7 +195,7 @@ class RamanSolver:
     def _int_spontaneous_raman(self, z_array, raman_matrix, alphap_fiber, freq_array, cr_raman_matrix, freq_diff, ase_bc, bn_array, temperature):
         spontaneous_raman_scattering = OptimizeResult()
 
-        dx = self.solver_params.z_resolution
+        dx = self.raman_params.space_resolution
         h = ph.value('Planck constant')
         kb = ph.value('Boltzmann constant')
 
@@ -227,42 +226,37 @@ class RamanSolver:
 
         return spontaneous_raman_scattering
 
-    @property
-    def stimulated_raman_scattering(self):
-        """ Return rho fiber gain/loss profile induced by stimulated Raman scattering.
-        :return: self._stimulated_raman_scattering: the SRS problem solution.
+    def stimulated_raman_scattering(self, carriers, raman_pumps=None):
+        """ Returns stimulated Raman scattering solution including 
+        fiber gain/loss profile.
+        :return: self.__stimulated_raman_scattering: the SRS problem solution.
         scipy.interpolate.PPoly instance
         """
 
-        if self._stimulated_raman_scattering is None:
-            fiber_length = self.fiber_information.length
-            attenuation_coefficient = self.fiber_information.attenuation_coefficient
-            raman_coefficient = self.fiber_information.raman_coefficient
-
-            spectral_info = self.spectral_information
-            raman_pump_information = self.raman_pump_information
-
-            z_resolution = self.solver_params.z_resolution
-            tolerance = self.solver_params.tolerance
-            verbose = self.solver_params.verbose
+        if self.__stimulated_raman_scattering is None:
+            # fiber parameters
+            fiber_length = self.fiber_params.length
+            loss_coef = self.fiber_params.loss_coef
+            raman_efficiency = self.fiber_params.raman_efficiency
+            # raman solver parameters
+            z_resolution = self.raman_params.space_resolution
+            tolerance = self.raman_params.tolerance
+            verbose = self.raman_params.verbose
 
             if verbose:
                 print('Start computing fiber Stimulated Raman Scattering')
 
-            power_spectrum, freq_array, prop_direct, _ = self._compute_power_spectrum(spectral_info, raman_pump_information)
+            power_spectrum, freq_array, prop_direct, _ = self._compute_power_spectrum(carriers, raman_pumps)
 
-            if len(attenuation_coefficient.alpha_power) >= 2:
-                interp_alphap = interp1d(attenuation_coefficient.frequency, attenuation_coefficient.alpha_power)
-                alphap_fiber = interp_alphap(freq_array)
+            if not hasattr(loss_coef, 'alpha_power'):
+                alphap_fiber = loss_coef * np.ones(freq_array.shape)
             else:
-                alphap_fiber = attenuation_coefficient.alpha_power * np.ones(freq_array.shape)
+                interp_alphap = interp1d(loss_coef['frequency'], loss_coef['alpha_power'])
+                alphap_fiber = interp_alphap(freq_array)
 
             freq_diff = abs(freq_array - np.reshape(freq_array, (len(freq_array), 1)))
-            if len(raman_coefficient.cr) >= 2:
-                interp_cr = interp1d(raman_coefficient.frequency, raman_coefficient.cr)
-                cr = interp_cr(freq_diff)
-            else:
-                cr = raman_coefficient.cr * np.ones(freq_diff.shape)
+            interp_cr = interp1d(raman_efficiency['frequency_offset'], raman_efficiency['cr'])
+            cr = interp_cr(freq_diff)
 
             # z propagation axis
             z = np.arange(0, fiber_length+1, z_resolution)
@@ -276,7 +270,6 @@ class RamanSolver:
 
             rho = (stimulated_raman_scattering.y.transpose() / power_spectrum).transpose()
             rho = np.sqrt(rho)    # From power attenuation to field attenuation
-
             setattr(stimulated_raman_scattering, 'frequency', freq_array)
             setattr(stimulated_raman_scattering, 'z', stimulated_raman_scattering.x)
             setattr(stimulated_raman_scattering, 'rho', rho)
@@ -284,9 +277,11 @@ class RamanSolver:
             delattr(stimulated_raman_scattering, 'x')
             delattr(stimulated_raman_scattering, 'y')
 
-            self._stimulated_raman_scattering = stimulated_raman_scattering
+            self.carriers = carriers
+            self.raman_pumps = raman_pumps
+            self.__stimulated_raman_scattering = stimulated_raman_scattering
 
-        return self._stimulated_raman_scattering
+        return self.__stimulated_raman_scattering
 
     def _residuals_stimulated_raman(self, ya, yb, power_spectrum, prop_direct):
 
@@ -345,112 +340,3 @@ class RamanSolver:
                 dpdz[f_ind][z_ind] = dpdz_element
 
         return np.vstack(dpdz)
-
-class NLI:
-    """ This class implements the NLI models.
-        Model and method can be specified in `self.model_parameters.method`.
-        List of implemented methods:
-        'gn_analytic': brute force triple integral solution
-        'GGN_spectrally_separated_xpm_spm': XPM plus SPM
-    """
-
-    def __init__(self, fiber_information=None):
-        """ Initialize the fiber object with its physical parameters
-        """
-        self.fiber_information = fiber_information
-        self.srs_profile = None
-        self.model_parameters = None
-
-    @property
-    def fiber_information(self):
-        return self.__fiber_information
-
-    @fiber_information.setter
-    def fiber_information(self, fiber_information):
-        self.__fiber_information = fiber_information
-
-    @property
-    def srs_profile(self):
-        return self.__srs_profile
-
-    @srs_profile.setter
-    def srs_profile(self, srs_profile):
-        self.__srs_profile = srs_profile
-
-    @property
-    def model_parameters(self):
-        return self.__model_parameters
-
-    @model_parameters.setter
-    def model_parameters(self, model_params):
-        """
-        :param model_params: namedtuple containing the parameters used to compute the NLI.
-        """
-        self.__model_parameters = model_params
-
-    def alpha0(self, f_eval=193.5e12):
-        if len(self.fiber_information.attenuation_coefficient.alpha_power) == 1:
-            alpha0 = self.fiber_information.attenuation_coefficient.alpha_power[0]
-        else:
-            alpha_interp = interp1d(self.fiber_information.attenuation_coefficient.frequency,
-                                    self.fiber_information.attenuation_coefficient.alpha_power)
-            alpha0 = alpha_interp(f_eval)
-        return alpha0
-
-    def compute_nli(self, carrier, *carriers):
-        """ Compute NLI power generated by the WDM comb `*carriers` on the channel under test `carrier`
-        at the end of the fiber span.
-        """
-        if 'gn_model_analytic' == self.model_parameters.method.lower():
-            carrier_nli = self._gn_analytic(carrier, *carriers)
-        else:
-            raise ValueError(f'Method {self.model_parameters.method_nli} not implemented.')
-
-        return carrier_nli
-
-    # Methods for computing spectrally separated GN
-    def _gn_analytic(self, carrier, *carriers):
-        """ Computes the nonlinear interference power on a single carrier.
-        The method uses eq. 120 from arXiv:1209.0394.
-        :param carrier: the signal under analysis
-        :param carriers: the full WDM comb
-        :return: carrier_nli: the amount of nonlinear interference in W on the under analysis
-        """
-
-        alpha = self.alpha0() / 2
-        length = self.fiber_information.length
-        effective_length = (1 - np.exp(-2 * alpha * length)) / (2 * alpha)
-        asymptotic_length = 1 / (2 * alpha)
-
-        beta2 = self.fiber_information.beta2
-        gamma = self.fiber_information.gamma
-
-        g_nli = 0
-        for interfering_carrier in carriers:
-            g_interfearing = interfering_carrier.power.signal / interfering_carrier.baud_rate
-            g_signal = carrier.power.signal / carrier.baud_rate
-            g_nli += g_interfearing**2 * g_signal * self._psi(carrier, interfering_carrier)
-        g_nli *= (16.0 / 27.0) * (gamma * effective_length)**2 /\
-                 (2 * np.pi * abs(beta2) * asymptotic_length)
-
-        carrier_nli = carrier.baud_rate * g_nli
-        return carrier_nli
-
-    def _psi(self, carrier, interfering_carrier):
-        """ Calculates eq. 123 from arXiv:1209.0394.
-        """
-        alpha = self.alpha0() / 2
-        beta2 = self.fiber_information.beta2
-
-        asymptotic_length = 1 / (2 * alpha)
-        if carrier.channel_number == interfering_carrier.channel_number:  # SPM
-            psi = np.arcsinh(0.5 * np.pi**2 * asymptotic_length
-                              * abs(beta2) * carrier.baud_rate**2)
-        else:  # XPM
-            delta_f = carrier.frequency - interfering_carrier.frequency
-            psi = np.arcsinh(np.pi**2 * asymptotic_length * abs(beta2) *
-                             carrier.baud_rate * (delta_f + 0.5 * interfering_carrier.baud_rate))
-            psi -= np.arcsinh(np.pi**2 * asymptotic_length * abs(beta2) *
-                              carrier.baud_rate * (delta_f - 0.5 * interfering_carrier.baud_rate))
-
-        return psi
