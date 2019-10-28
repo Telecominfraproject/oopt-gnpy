aafd82b16d22a577873970cc3579f5f531f3d631
Jan Kundrát <jan.kundrat@telecominfraproject.com>
2019-10-28 17:02:08 +0100

    Docker: run the TIP Summit 2019 demo by default
    Here's a TL;DR of how to use this. First, start the container so that it
    exports its port 5000 for incoming HTTP requests:
    
    - docker run -P -it --rm telecominfraproject/oopt-gnpy-experimental:$SOME_VERSION
    
    You'll have to replace `$SOME_VERSION` by acn actual version. There is
    no default `latest` tag on this particular Docker repository.
    
    Then find out what port Docker currently uses:
    
    ```console-session
    ~ # docker ps
    CONTAINER ID        IMAGE                                                          COMMAND                  CREATED             STATUS              PORTS                     NAMES
    6004c3a9b741        telecominfraproject/oopt-gnpy-experimental:v1.8-114-g7e0abc4   "/oopt-gnpy/.docker-…"   48 seconds ago      Up 44 seconds       0.0.0.0:32768->5000/tcp   eloquent_hawking
    ~ # docker port 6004c3a9b741
    5000/tcp -> 0.0.0.0:32768
    ```
    
    Path computation can then be requested like this:
    
    - curl -v -X POST -H "Content-Type: application/json" -d @examples/2019-demo-services.json http://127.0.0.1:32768/gnpy-experimental
    
    This one will try to compute two disjoint optical paths and output their
    respective optical performance.

--- .docker-travis.sh
+++ .docker-travis.sh
@@ -5,17 +5,16 @@ set -e
 IMAGE_NAME=telecominfraproject/oopt-gnpy
 IMAGE_TAG=$(git describe --tags)
 
+if [[ "${TRAVIS_BRANCH}" == "experimental/2019-summit" ]]; then
+  IMAGE_NAME=telecominfraproject/oopt-gnpy-experimental
+fi
+
 ALREADY_FOUND=0
 docker pull ${IMAGE_NAME}:${IMAGE_TAG} && ALREADY_FOUND=1
 
 if [[ $ALREADY_FOUND == 0 ]]; then
   docker build . -t ${IMAGE_NAME}
   docker tag ${IMAGE_NAME} ${IMAGE_NAME}:${IMAGE_TAG}
-
-  # shared directory setup: do not clobber the real data
-  mkdir trash
-  cd trash
-  docker run -it --rm --volume $(pwd):/shared ${IMAGE_NAME} ./transmission_main_example.py
 else
   echo "Image ${IMAGE_NAME}:${IMAGE_TAG} already available, will just update the other tags"
 fi
@@ -43,5 +42,11 @@ if [[ "${TRAVIS_PULL_REQUEST}" == "false" ]]; then
       docker push ${IMAGE_NAME}:${IMAGE_TAG}
     fi
     docker push ${IMAGE_NAME}:stable
+  elif [[ "${TRAVIS_BRANCH}" == "experimental/2019-summit" ]]; then
+    echo "Publishing ad-hoc image for the TIP Summit demo"
+    do_docker_login
+    if [[ $ALREADY_FOUND == 0 ]]; then
+      docker push ${IMAGE_NAME}:${IMAGE_TAG}
+    fi
   fi
 fi
--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,7 @@ FROM python:3.7-slim
 COPY . /oopt-gnpy
 WORKDIR /oopt-gnpy
 RUN python setup.py install
-WORKDIR /shared/examples
+WORKDIR /shared
 ENTRYPOINT ["/oopt-gnpy/.docker-entry.sh"]
-CMD ["/bin/bash"]
+CMD ["python", "examples/path_requests_run.py", "examples/2019-demo-topology.json", "examples/2019-demo-services.json", "examples/2019-demo-equipment.json", "--rest"]
+EXPOSE 5000
