d56c91ca7fa2901312f96294914b6d6fd3d58b20
EstherLerouzic <esther.lerouzic@orange.com>
2020-02-19 14:54:16 +0100

    small linter fixes
    (Jan: cherry-picked relevant parts of c6be21f36fe2da3ee7aa1cf22e2d2ba062f1313b)
    
    Signed-off-by: EstherLerouzic <esther.lerouzic@orange.com>
    Change-Id: Ib84f452a15a20a88ea186ceef64712eb967d46a7

--- tests/test_disjunction.py
+++ tests/test_disjunction.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 # TelecomInfraProject/gnpy/examples
 # Module name : test_disjunction.py
-# Version : 
-# License : BSD 3-Clause Licence
+# Version:
+# License: BSD 3-Clause Licence
 # Copyright (c) 2018, Telecom Infra Project
 
 """
@@ -24,10 +24,10 @@ from gnpy.core.utils import lin2db
 from gnpy.core.elements import Roadm
 from gnpy.core.spectrum_assignment import build_oms_list
 
-network_file_name = Path(__file__).parent.parent / 'tests/data/testTopology_expected.json'
-service_file_name = Path(__file__).parent.parent / 'tests/data/testTopology_testservices.json'
-result_file_name  = Path(__file__).parent.parent / 'tests/data/testTopology_testresults.json'
-eqpt_library_name = Path(__file__).parent.parent / 'tests/data/eqpt_config.json'
+NETWORK_FILE_NAME = Path(__file__).parent.parent / 'tests/data/testTopology_expected.json'
+SERVICE_FILE_NAME = Path(__file__).parent.parent / 'tests/data/testTopology_testservices.json'
+RESULT_FILE_NAME = Path(__file__).parent.parent / 'tests/data/testTopology_testresults.json'
+EQPT_LIBRARY_NAME = Path(__file__).parent.parent / 'tests/data/eqpt_config.json'
 
 @pytest.fixture()
 def serv(test_setup):
@@ -48,9 +48,9 @@ def test_setup():
     network = load_network(NETWORK_FILE_NAME, equipment)
     # Build the network once using the default power defined in SI in eqpt config
     # power density : db2linp(ower_dbm": 0)/power_dbm": 0 * nb channels as defined by
-    # spacing, f_min and f_max 
+    # spacing, f_min and f_max
     p_db = equipment['SI']['default'].power_dbm
-    
+
     p_total_db = p_db + lin2db(automatic_nch(equipment['SI']['default'].f_min,\
         equipment['SI']['default'].f_max, equipment['SI']['default'].spacing))
     build_network(network, equipment, p_db, p_total_db)
@@ -66,7 +66,7 @@ def test_disjunction(serv):
     pths = compute_path_dsjctn(network, equipment, rqs, dsjn)
     print(dsjn)
 
-    dsjn_list = [d.disjunctions_req for d in dsjn ]
+    dsjn_list = [d.disjunctions_req for d in dsjn]
 
     # assumes only pairs in dsjn list
     test = True
@@ -88,21 +88,19 @@ def test_does_not_loop_back(serv):
     network, equipment, rqs, dsjn = serv
     pths = compute_path_dsjctn(network, equipment, rqs, dsjn)
     test = True
-    for p in pths :
+    for p in pths:
         for el in p:
             p.remove(el)
             a = [e for e in p if e.uid == el.uid]
-            if a :
+            if a:
                 test = False
                 break
+    assert test
 
-    assert test        
-
-    # TODO : test that identical requests are correctly agregated 
+    # TODO : test that identical requests are correctly agregated
     # and reproduce disjunction vector as well as route constraints
     # check that requests with different parameters are not aggregated
     # check that the total agregated bandwidth is the same after aggregation
-
     #
 
 def create_rq(equipment, srce, dest, bdir, nd_list, ls_list):
