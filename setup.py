4d5d10935a41fef9db10cdd9106de2a76de74e04
Jan Kundrát <jan.kundrat@telecominfraproject.com>
2020-03-24 21:14:13 +0100

    Convert to `pbr` setup
    It turns out that our current setup does not really support the `sdist`
    Python packaging step. I'm trying to increase automation, both for
    making releases for upload to pypi.org, and also for CI via Zuul. As it
    turns out, Zuul comes with a set of predefined jobs which -- by default
    -- use `tox`, and tox was having troubles dealing with our `setup.py`
    because it also assumes that the `sdist` step works. It is also supposed
    to:
    
    - fix version number duplication in `setup.py`,
    - fix version number duplication in Sphinx docs,
    - prevent the need to write a `MANIFEST.in` manually.
    
    TL;DR: insetad of having to deal with a ton of other creative
    boilerplate, use tools for lazy people like me, and PBR ("Python Build
    Reasonableness") appears to check the marks here.
    
    Change-Id: I27c36c4f6b0e76857d16f7819ec237e9b811476a

--- setup.py
+++ setup.py
@@ -1,39 +1,5 @@
 #!/usr/bin/python3
 
-from codecs import open
-from os import path
-from setuptools import setup, find_packages
+import setuptools
 
-here = path.abspath(path.dirname(__file__))
-
-with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
-    long_description = f.read()
-
-setup(
-    name='gnpy',
-    version='2.1',
-    description='route planning and optimization tool for mesh optical networks',
-    long_description=long_description,
-    long_description_content_type='text/x-rst; charset=UTF-8',
-    url='https://github.com/Telecominfraproject/gnpy',
-    author='Telecom Infra Project',
-    author_email='jan.kundrat@telecominfraproject.com',
-    classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
-        'Intended Audience :: Science/Research',
-        'Intended Audience :: Telecommunications Industry',
-        'License :: OSI Approved :: BSD License',
-        'Natural Language :: English',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3 :: Only',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Topic :: Scientific/Engineering',
-        'Topic :: Scientific/Engineering :: Physics',
-    ],
-    keywords='optics network fiber communication route planning optimization',
-    #packages=find_packages(exclude=['examples', 'docs', 'tests']),  # Required
-    packages=find_packages(exclude=['docs', 'tests']),  # Required
-    install_requires=list(open('requirements.txt'))
-)
+setuptools.setup(setup_requires=['pbr'], pbr=True)
