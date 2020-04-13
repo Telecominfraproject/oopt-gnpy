b65bedf16fb19730af88c1bb3b15728c8627a913
Jan Kundrát <jan.kundrat@telecominfraproject.com>
2020-04-13 13:55:05 +0200

    tests: Check if example code provides exact same output
    Change-Id: I5938f85337e4254092683dadc806a0a419cb2a04

--- tests/test_invocation.py
+++ tests/test_invocation.py
@@ -1,13 +1,21 @@
 # -*- coding: utf-8 -*-
 
+from pathlib import Path
 import pytest
 import subprocess
 
-@pytest.mark.parametrize("invocation", (
-    ('./examples/transmission_main_example.py',),
-    ('./examples/path_requests_run.py',),
-    ('./examples/transmission_main_example.py', 'examples/raman_edfa_example_network.json', '--sim', 'examples/sim_params.json', '--show-channels',),
+@pytest.mark.parametrize("output, invocation", (
+    ('transmission_main_example',
+        ('./examples/transmission_main_example.py',)),
+    ('path_request_run',
+        ('./examples/path_requests_run.py',)),
+    ('transmission_main_example__raman',
+        ('./examples/transmission_main_example.py', 'examples/raman_edfa_example_network.json',
+         '--sim', 'examples/sim_params.json', '--show-channels',)),
 ))
-def test_example_invocation(invocation):
+def test_example_invocation(output, invocation):
     '''Make sure that our examples produce useful output'''
-    proc = subprocess.run(invocation, check=True)
+    expected = open(Path(__file__).parent / 'invocation' / output, mode='rb').read()
+    proc = subprocess.run(invocation, stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=True)
+    assert proc.stderr == b''
+    assert proc.stdout == expected
