81289c161d1ffc65f8789be78a23af661886e57d
Alessio Ferrari <alessio.ferrari@polito.it>
2020-05-21 11:00:17 +0200

    Introduce computation of the chromatic dispersion
    Change-Id: I3ee039154568d4255444fa8db5e89945851010f4

--- gnpy/core/info.py
+++ gnpy/core/info.py
@@ -17,8 +17,16 @@ class Power(namedtuple('Power', 'signal nli ase')):
     """carriers power in W"""
 
 
-class Channel(namedtuple('Channel', 'channel_number frequency baud_rate roll_off power')):
-    pass
+class Channel(namedtuple('Channel', 'channel_number frequency baud_rate roll_off power chromatic_dispersion')):
+    """ Class containing the parameters of a WDM signal.
+
+        :param channel_number: channel number in the WDM grid
+        :param frequency: central frequency of the signal (Hz)
+        :param baud_rate: the symbol rate of the signal (Baud)
+        :param roll_off: the roll off of the signal. It is a pure number between 0 and 1
+        :param power (gnpy.core.info.Power): power of signal, ASE noise and NLI (W)
+        :param chromatic_dispersion: chromatic dispersion (s/m)
+    """
 
 
 class Pref(namedtuple('Pref', 'p_span0, p_spani, neq_ch ')):
@@ -42,6 +50,7 @@ def create_input_spectral_information(f_min, f_max, roll_off, baud_rate, power, 
         pref=Pref(pref, pref, lin2db(nb_channel)),
         carriers=[
             Channel(f, (f_min + spacing * f),
-                    baud_rate, roll_off, Power(power, 0, 0)) for f in range(1, nb_channel + 1)
-        ])
+                    baud_rate, roll_off, Power(power, 0, 0), 0) for f in range(1, nb_channel + 1)
+        ]
+    )
     return si
