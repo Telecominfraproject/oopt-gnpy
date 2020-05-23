b19cede47554e0a86267a47c2e575697a2521fec
Jan Kundrát <jan.kundrat@telecominfraproject.com>
2020-05-23 21:02:45 +0200

    SimParams: pretty exceptions upon a misconfiguration
    Change-Id: I69f47925dd08c492b295b6824618a4595d142954

--- gnpy/core/parameters.py
+++ gnpy/core/parameters.py
@@ -115,14 +115,17 @@ class NLIParams(Parameters):
 
 class SimParams(Parameters):
     def __init__(self, **kwargs):
-        if 'nli_parameters' in kwargs:
-            self._nli_params = NLIParams(**kwargs['nli_parameters'])
-        else:
-            self._nli_params = None
-        if 'raman_parameters' in kwargs:
-            self._raman_params = RamanParams(**kwargs['raman_parameters'])
-        else:
-            self._raman_params = None
+        try:
+            if 'nli_parameters' in kwargs:
+                self._nli_params = NLIParams(**kwargs['nli_parameters'])
+            else:
+                self._nli_params = None
+            if 'raman_parameters' in kwargs:
+                self._raman_params = RamanParams(**kwargs['raman_parameters'])
+            else:
+                self._raman_params = None
+        except KeyError as e:
+            raise ParametersError(f'Simulation parameters must include {e}. Configuration: {kwargs}')
 
     @property
     def nli_params(self):
