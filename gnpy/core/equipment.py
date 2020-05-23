a094568d6ee0a52ed1a9615376b2b97cdd894b90
Jan Kundrát <jan.kundrat@telecominfraproject.com>
2020-05-23 16:32:00 +0200

    equipment: move NF estimation into science_utils
    I envision equipment.py as something which deals exclusively with the
    traditional GNPy's JSON-formatted data, so make sure we do not include
    any computation in that file.
    
    Change-Id: I8473cccd84243147181a7195ba39fc6c9db3c42f

--- gnpy/core/equipment.py
+++ gnpy/core/equipment.py
@@ -8,14 +8,13 @@ gnpy.core.equipment
 This module contains functionality for specifying equipment.
 '''
 
-from numpy import clip
-from math import isclose
 from pathlib import Path
 from json import load
-from gnpy.core.utils import automatic_nch, lin2db, db2lin, load_json
+from gnpy.core.utils import automatic_nch, db2lin, load_json
 from collections import namedtuple
 from gnpy.core import ansi_escapes
 from gnpy.core.exceptions import EquipmentConfigError
+from gnpy.core.science_utils import estimate_nf_model
 import time
 
 Model_vg = namedtuple('Model_vg', 'nf1 nf2 delta_p')
@@ -183,7 +182,7 @@ class Amp(common):
                 del kwargs['nf0']
             except KeyError:
                 pass  # nf0 is not needed for variable gain amp
-            nf1, nf2, delta_p = nf_model(type_variety, gain_min, gain_max, nf_min, nf_max)
+            nf1, nf2, delta_p = estimate_nf_model(type_variety, gain_min, gain_max, nf_min, nf_max)
             nf_def = Model_vg(nf1, nf2, delta_p)
         elif type_def == 'openroadm':
             try:
@@ -204,52 +203,6 @@ class Amp(common):
 
         return cls(**{**kwargs, **json_data,
                       'nf_model': nf_def, 'dual_stage_model': dual_stage_def})
-
-
-def nf_model(type_variety, gain_min, gain_max, nf_min, nf_max):
-    if nf_min < -10:
-        raise EquipmentConfigError(f'Invalid nf_min value {nf_min!r} for amplifier {type_variety}')
-    if nf_max < -10:
-        raise EquipmentConfigError(f'Invalid nf_max value {nf_max!r} for amplifier {type_variety}')
-
-    # NF estimation model based on nf_min and nf_max
-    # delta_p:  max power dB difference between first and second stage coils
-    # dB g1a:   first stage gain - internal VOA attenuation
-    # nf1, nf2: first and second stage coils
-    #           calculated by solving nf_{min,max} = nf1 + nf2 / g1a{min,max}
-    delta_p = 5
-    g1a_min = gain_min - (gain_max - gain_min) - delta_p
-    g1a_max = gain_max - delta_p
-    nf2 = lin2db((db2lin(nf_min) - db2lin(nf_max)) /
-                 (1 / db2lin(g1a_max) - 1 / db2lin(g1a_min)))
-    nf1 = lin2db(db2lin(nf_min) - db2lin(nf2) / db2lin(g1a_max))
-
-    if nf1 < 4:
-        raise EquipmentConfigError(f'First coil value too low {nf1} for amplifier {type_variety}')
-
-    # Check 1 dB < delta_p < 6 dB to ensure nf_min and nf_max values make sense.
-    # There shouldn't be high nf differences between the two coils:
-    #    nf2 should be nf1 + 0.3 < nf2 < nf1 + 2
-    # If not, recompute and check delta_p
-    if not nf1 + 0.3 < nf2 < nf1 + 2:
-        nf2 = clip(nf2, nf1 + 0.3, nf1 + 2)
-        g1a_max = lin2db(db2lin(nf2) / (db2lin(nf_min) - db2lin(nf1)))
-        delta_p = gain_max - g1a_max
-        g1a_min = gain_min - (gain_max - gain_min) - delta_p
-        if not 1 < delta_p < 11:
-            raise EquipmentConfigError(f'Computed \N{greek capital letter delta}P invalid \
-                \n 1st coil vs 2nd coil calculated DeltaP {delta_p:.2f} for \
-                \n amplifier {type_variety} is not valid: revise inputs \
-                \n calculated 1st coil NF = {nf1:.2f}, 2nd coil NF = {nf2:.2f}')
-    # Check calculated values for nf1 and nf2
-    calc_nf_min = lin2db(db2lin(nf1) + db2lin(nf2) / db2lin(g1a_max))
-    if not isclose(nf_min, calc_nf_min, abs_tol=0.01):
-        raise EquipmentConfigError(f'nf_min does not match calc_nf_min, {nf_min} vs {calc_nf_min} for amp {type_variety}')
-    calc_nf_max = lin2db(db2lin(nf1) + db2lin(nf2) / db2lin(g1a_min))
-    if not isclose(nf_max, calc_nf_max, abs_tol=0.01):
-        raise EquipmentConfigError(f'nf_max does not match calc_nf_max, {nf_max} vs {calc_nf_max} for amp {type_variety}')
-
-    return nf1, nf2, delta_p
 
 
 def trx_mode_params(equipment, trx_type_variety='', trx_mode='', error_message=False):
--- gnpy/core/science_utils.py
+++ gnpy/core/science_utils.py
@@ -6,8 +6,10 @@ from scipy.integrate import solve_bvp
 from scipy.integrate import cumtrapz
 from scipy.interpolate import interp1d
 from scipy.optimize import OptimizeResult
+from math import isclose
 
-from gnpy.core.utils import db2lin
+from gnpy.core.utils import db2lin, lin2db
+from gnpy.core.exceptions import EquipmentConfigError
 
 
 logger = getLogger(__name__)
@@ -682,3 +684,49 @@ def _psi(carrier, interfering_carrier, beta2, asymptotic_length):
         psi -= np.arcsinh(np.pi**2 * asymptotic_length * abs(beta2) *
                           carrier.baud_rate * (delta_f - 0.5 * interfering_carrier.baud_rate))
     return psi
+
+
+def estimate_nf_model(type_variety, gain_min, gain_max, nf_min, nf_max):
+    if nf_min < -10:
+        raise EquipmentConfigError(f'Invalid nf_min value {nf_min!r} for amplifier {type_variety}')
+    if nf_max < -10:
+        raise EquipmentConfigError(f'Invalid nf_max value {nf_max!r} for amplifier {type_variety}')
+
+    # NF estimation model based on nf_min and nf_max
+    # delta_p:  max power dB difference between first and second stage coils
+    # dB g1a:   first stage gain - internal VOA attenuation
+    # nf1, nf2: first and second stage coils
+    #           calculated by solving nf_{min,max} = nf1 + nf2 / g1a{min,max}
+    delta_p = 5
+    g1a_min = gain_min - (gain_max - gain_min) - delta_p
+    g1a_max = gain_max - delta_p
+    nf2 = lin2db((db2lin(nf_min) - db2lin(nf_max)) /
+                 (1 / db2lin(g1a_max) - 1 / db2lin(g1a_min)))
+    nf1 = lin2db(db2lin(nf_min) - db2lin(nf2) / db2lin(g1a_max))
+
+    if nf1 < 4:
+        raise EquipmentConfigError(f'First coil value too low {nf1} for amplifier {type_variety}')
+
+    # Check 1 dB < delta_p < 6 dB to ensure nf_min and nf_max values make sense.
+    # There shouldn't be high nf differences between the two coils:
+    #    nf2 should be nf1 + 0.3 < nf2 < nf1 + 2
+    # If not, recompute and check delta_p
+    if not nf1 + 0.3 < nf2 < nf1 + 2:
+        nf2 = np.clip(nf2, nf1 + 0.3, nf1 + 2)
+        g1a_max = lin2db(db2lin(nf2) / (db2lin(nf_min) - db2lin(nf1)))
+        delta_p = gain_max - g1a_max
+        g1a_min = gain_min - (gain_max - gain_min) - delta_p
+        if not 1 < delta_p < 11:
+            raise EquipmentConfigError(f'Computed \N{greek capital letter delta}P invalid \
+                \n 1st coil vs 2nd coil calculated DeltaP {delta_p:.2f} for \
+                \n amplifier {type_variety} is not valid: revise inputs \
+                \n calculated 1st coil NF = {nf1:.2f}, 2nd coil NF = {nf2:.2f}')
+    # Check calculated values for nf1 and nf2
+    calc_nf_min = lin2db(db2lin(nf1) + db2lin(nf2) / db2lin(g1a_max))
+    if not isclose(nf_min, calc_nf_min, abs_tol=0.01):
+        raise EquipmentConfigError(f'nf_min does not match calc_nf_min, {nf_min} vs {calc_nf_min} for amp {type_variety}')
+    calc_nf_max = lin2db(db2lin(nf1) + db2lin(nf2) / db2lin(g1a_min))
+    if not isclose(nf_max, calc_nf_max, abs_tol=0.01):
+        raise EquipmentConfigError(f'nf_max does not match calc_nf_max, {nf_max} vs {calc_nf_max} for amp {type_variety}')
+
+    return nf1, nf2, delta_p
