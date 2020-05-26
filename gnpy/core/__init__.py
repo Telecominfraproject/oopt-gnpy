1bcf7ed0bd05cfb31a72c552ebc902566ab50d5f
Jan Kundrát <jan.kundrat@telecominfraproject.com>
2020-05-26 18:14:24 +0200

    docs: basic stuff about the module structure
    As a bonus, in the Python shell, `help(gnpy)`, `help(gnpy.core)`, etc,
    now produce at least some useful information.
    
    Change-Id: I76ade6f2456fcebd3c0a147374815dd245dc4b10

---
+++ gnpy/core/__init__.py
@@ -0,0 +1,9 @@
+'''
+Simulation of signal propagation in the DWDM network
+
+Optical signals, as defined via :class:`.info.SpectralInformation`, enter
+:py:mod:`.elements` which compute how these signals are affected as they travel
+through the :py:mod:`.network`.
+The simulation is controlled via :py:mod:`.parameters` and implemented mainly
+via :py:mod:`.science_utils`.
+'''
