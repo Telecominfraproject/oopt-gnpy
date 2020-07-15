f4ab38452ba2fbbc308dafef4cea5983c8586563
Rodrigo Sasse David <rodrigo.sassedavid@orange.com>
2020-07-15 15:38:40 +0200

    Test code behaviour in case of route constraint (loose and strict)..
    Signed-off-by: Rodrigo Sasse David <rodrigo.sassedavid@orange.com>
    Change-Id: If88633f43f46e9f472c48c69702245588b4e7ae7

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -541,3 +541,19 @@ def test_mode_low_snr(xls_input):
     #differences from results without 'forced modes'    
     assert len(propagated_paths[1]) == 0
     assert len(reversed_paths[2]) != 0
+
+
+@pytest.mark.parametrize('xls_input', 
+    (DATA_DIR / 'test_path_route_constraint.xls', )
+)
+def test_compute_route_constraint(xls_input):
+    """Test code behaviour in case of route constraint (loose and strict)."""
+    equipment = load_equipment(EQPT_FILENAME)
+    network = load_network(xls_input, equipment)
+    service = read_service_sheet(xls_input, equipment, network)
+    oms_list = build_oms_list(network, equipment)
+    requests = requests_from_json(service, equipment)
+    disjunctions = deduplicate_disjunctions(disjunctions_from_json(service))
+    requests, disjunctions = requests_aggregation(requests, disjunctions)
+    with pytest.raises(DisjunctionError):
+        paths = compute_path_dsjctn(network, equipment, requests, disjunctions)
