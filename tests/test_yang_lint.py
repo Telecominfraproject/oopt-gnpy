9229ed38f3f85363af3d888992185d306f2f09af
Jan Kundrát <jan.kundrat@telecominfraproject.com>
2020-07-26 16:33:00 +0200

    YANG: Prepare for distributing YANG modules
    I'm using a second-level module namespace (gnpy.yang) for the same
    reasons as when shipping the example data via gnpy/example-data/ --
    these will be used in the subsequent commits when we actually start
    adding YANG models. There is also some code (not much now, a lot more in
    future) for working with these models, and in future also for loading
    actual data. These *could* be put into gnpy.tools.*, but I think it's
    more straightforward to just keep them in the YANG namespace.
    
    This is now using the Python bindings for the C++ wrapper around the C
    libyang library. Note that we could not have used pyang because it just
    does not implement actual YANG *data* validation/parsing/defaults/...
    natively.
    
    Change-Id: Ic40738ddd8346429bde01e591d19fd2ce8cb687d

---
+++ tests/test_yang_lint.py
@@ -0,0 +1,30 @@
+# SPDX-License-Identifier: BSD-3-Clause
+#
+# Tests for YANG models of GNPy
+#
+# Copyright (C) 2020-2022 Telecom Infra Project and GNPy contributors
+# see LICENSE.md for a list of contributors
+#
+
+from gnpy.yang import external_path, model_path
+import os
+from pathlib import Path
+from typing import List
+import pytest
+import oopt_gnpy_libyang as ly
+
+
+def _get_basename(filename: Path) -> str:
+    try:
+        return filename.name
+    except AttributeError:
+        return filename
+
+
+@pytest.mark.parametrize("yang_model", [x for x in external_path().glob('*.yang')] + [x for x in model_path().glob('*.yang')], ids=_get_basename)
+def test_lint_yang(yang_model):
+    '''Run a linter on each YANG model'''
+    c = ly.Context(str(external_path()) + os.pathsep + str(model_path()),
+                   ly.ContextOptions.NoYangLibrary | ly.ContextOptions.DisableSearchCwd)
+    assert c.parse_module(yang_model, ly.SchemaFormat.YANG) is not None
+    assert c.errors() == []
