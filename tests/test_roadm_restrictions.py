643680ec476f7072902e1fa7a8fa41c7dc72e557
EstherLerouzic <esther.lerouzic@orange.com>
2020-09-07 14:05:03 +0200

    add a set of test to check power equalization of roadm
    Check that egress power of roadm is equal to target power if input power
    is greater than target power else, that it is equal to input power.
    Use a simple two hops A-B-C topology for the test where the prev_node
    in ROADM B is either an amplifier or a fused. If it is a fused, the target
    power can not be met.
    
    Signed-off-by: EstherLerouzic <esther.lerouzic@orange.com>
    Change-Id: I3388f060a4f364055d58c8ca7c2b59143f784fa8

--- tests/test_roadm_restrictions.py
+++ tests/test_roadm_restrictions.py
@@ -16,7 +16,10 @@ from gnpy.core.utils import lin2db, automatic_nch
 from gnpy.core.elements import Fused, Roadm, Edfa
 from gnpy.core.network import build_network
 from gnpy.tools.json_io import network_from_json, load_equipment, load_json, Amp
-
+from gnpy.core.equipment import trx_mode_params
+from gnpy.topology.request import PathRequest, compute_constrained_path
+from gnpy.core.info import create_input_spectral_information
+from gnpy.core.utils import db2lin
 
 TEST_DIR = Path(__file__).parent
 EQPT_LIBRARY_NAME = TEST_DIR / 'data/eqpt_config.json'
@@ -202,3 +205,71 @@ def test_restrictions(restrictions, equipment):
                     not roadms[next(network.successors(amp)).uid].restrictions['preamp_variety_list']:
                 if amp.params.type_variety not in restrictions['preamp_variety_list']:
                     raise AssertionError()
+
+
+@pytest.mark.parametrize('prev_node_type, effective_pch_out_db', [('edfa', -20.0), ('fused', -22.0)])
+def test_roadm_target_power(prev_node_type, effective_pch_out_db):
+    ''' Check that egress power of roadm is equal to target power if input power is greater
+    than target power else, that it is equal to input power. Use a simple two hops A-B-C topology
+    for the test where the prev_node in ROADM B is either an amplifier or a fused, so that the target
+    power can not be met in this last case.
+    '''
+    equipment = load_equipment(EQPT_LIBRARY_NAME)
+    json_network = load_json(TEST_DIR / 'data/twohops_roadm_power_test.json')
+    prev_node = next(n for n in json_network['elements'] if n['uid'] == 'west edfa in node B to ila2')
+    json_network['elements'].remove(prev_node)
+    if prev_node_type == 'edfa':
+        prev_node = {'uid': 'west edfa in node B to ila2', 'type': 'Edfa'}
+    elif prev_node_type == 'fused':
+        prev_node = {'uid': 'west edfa in node B to ila2', 'type': 'Fused'}
+        prev_node['params'] = {'loss': 0}
+    json_network['elements'].append(prev_node)
+    network = network_from_json(json_network, equipment)
+    # Build the network once using the default power defined in SI in eqpt config
+    p_db = equipment['SI']['default'].power_dbm
+    p_total_db = p_db + lin2db(automatic_nch(equipment['SI']['default'].f_min,
+                                             equipment['SI']['default'].f_max, 
+                                             equipment['SI']['default'].spacing))
+
+    build_network(network, equipment, p_db, p_total_db)
+
+    params = {}
+    params['request_id'] = 0
+    params['trx_type'] = ''
+    params['trx_mode'] = ''
+    params['source'] = 'trx node A'
+    params['destination'] = 'trx node C'
+    params['bidir'] = False
+    params['nodes_list'] = ['trx node C']
+    params['loose_list'] = ['strict']
+    params['format'] = ''
+    params['path_bandwidth'] = 100e9
+    trx_params = trx_mode_params(equipment)
+    params.update(trx_params)
+    req = PathRequest(**params)
+    path = compute_constrained_path(network, req)
+    si = create_input_spectral_information(
+        req.f_min, req.f_max, req.roll_off, req.baud_rate,
+        req.power, req.spacing)
+    for i, el in enumerate(path):
+        if isinstance(el, Roadm):
+            carriers_power_in_roadm = min([c.power.signal + c.power.nli + c.power.ase for c in si.carriers])
+            si = el(si, degree=path[i+1].uid)
+            if el.uid == 'roadm node B':
+                print('input', carriers_power_in_roadm)
+                assert el.effective_pch_out_db == effective_pch_out_db
+                for carrier in si.carriers:
+                    print(carrier.power.signal + carrier.power.nli + carrier.power.ase)
+                    power = carrier.power.signal + carrier.power.nli + carrier.power.ase
+                    if prev_node_type == 'edfa':
+                        # edfa prev_node sets input power to roadm to a high enough value:
+                        # Check that egress power of roadm is equal to target power
+                        assert power == pytest.approx(db2lin(effective_pch_out_db - 30), rel=1e-3)
+                    elif prev_node_type == 'fused':
+                        # fused prev_node does reamplfy power after fiber propagation, so input power
+                        # to roadm is low.
+                        # Check that egress power of roadm is equalized to the min carrier input power.
+                        assert power == pytest.approx(carriers_power_in_roadm, rel=1e-3)
+        else:
+            si = el(si)
+
