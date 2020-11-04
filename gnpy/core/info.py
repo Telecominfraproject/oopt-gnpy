79d1d5c0647284201951e474df857e586707fb07
AndreaDAmico <andrea.damico@polito.it>
2020-11-04 20:58:56 +0100

    New definition of spectral information
    It allows the definition of an arbitrary spectral information.
    It is fully back-compatible.
    
    Change-Id: Id050e9f0a0d30780a49ecfbe8b96271fe47bcedc

--- gnpy/core/info.py
+++ gnpy/core/info.py
@@ -9,7 +9,13 @@ This module contains classes for modelling :class:`SpectralInformation`.
 """
 
 from collections import namedtuple
+from collections.abc import Sized
+from numpy import argsort, mean, array, squeeze, append, ones, ceil, any, zeros, outer
+
 from gnpy.core.utils import automatic_nch, lin2db
+from gnpy.core.exceptions import InfoError
+
+BASE_SLOT_WIDTH = 12.5e9  # Hz
 
 
 class Power(namedtuple('Power', 'signal nli ase')):
@@ -36,21 +42,209 @@ class Pref(namedtuple('Pref', 'p_span0, p_spani, neq_ch ')):
     neq_ch: equivalent channel count in dB"""
 
 
-class SpectralInformation(namedtuple('SpectralInformation', 'pref carriers')):
+class SpectralInformation(object):
+    """ Class containing the parameters of the entire WDM comb."""
+    def __init__(self, frequency, baud_rate, slot_width, signal, nli, ase,
+                 roll_off, chromatic_dispersion, pmd):
+        indices = argsort(frequency)
+        self._frequency = frequency[indices]
+        self._df = outer(ones(frequency.shape), frequency) - outer(frequency, ones(frequency.shape))
+        self._number_of_channels = len(self._frequency)
+        self._slot_width = slot_width[indices]
+        self._baud_rate = baud_rate[indices]
+        if any(self._frequency[:-1] + self._slot_width[:-1] / 2 > self._frequency[1:] - self._slot_width[1:] / 2):
+            raise InfoError('Spectrum required slot widths larger than the frequency spectral distances.')
+        elif any(self._baud_rate > self._slot_width):
+            raise InfoError('Spectrum baud rate larger than the slot width.')
+        self._signal = signal[indices]
+        self._nli = nli[indices]
+        self._ase = ase[indices]
+        self._roll_off = roll_off[indices]
+        self._chromatic_dispersion = chromatic_dispersion[indices]
+        self._pmd = pmd[indices]
+        self._channel_number = [*range(1, self._number_of_channels + 1)]
+        pref = lin2db(mean(signal) * 1e3)
+        self._pref = Pref(pref, pref, lin2db(self._number_of_channels))
 
-    def __new__(cls, pref, carriers):
-        return super().__new__(cls, pref, carriers)
+    @property
+    def pref(self):
+        return self._pref
+
+    @pref.setter
+    def pref(self, pref):
+        self._pref = pref
+
+    @property
+    def frequency(self):
+        return self._frequency
+
+    @property
+    def df(self):
+        return self._df
+
+    @property
+    def slot_width(self):
+        return self._slot_width
+
+    @property
+    def baud_rate(self):
+        return self._baud_rate
+
+    @property
+    def number_of_channels(self):
+        return self._number_of_channels
+
+    @property
+    def powers(self):
+        powers = zip(self.signal, self.nli, self.ase)
+        return [Power(*p) for p in powers]
+
+    @property
+    def signal(self):
+        return self._signal
+
+    @signal.setter
+    def signal(self, signal):
+        self._signal = signal
+
+    @property
+    def nli(self):
+        return self._nli
+
+    @nli.setter
+    def nli(self, nli):
+        self._nli = nli
+
+    @property
+    def ase(self):
+        return self._ase
+
+    @ase.setter
+    def ase(self, ase):
+        self._ase = ase
+
+    @property
+    def gsnr(self):
+        return lin2db(self.signal/(self.ase+self.nli))
+
+    @property
+    def osnr(self):
+        return lin2db(self.signal/self.ase)
+
+    @property
+    def snr(self):
+        return lin2db(self.signal/self.nli)
+
+    @property
+    def roll_off(self):
+        return self._roll_off
+
+    @property
+    def chromatic_dispersion(self):
+        return self._chromatic_dispersion
+
+    @chromatic_dispersion.setter
+    def chromatic_dispersion(self, chromatic_dispersion):
+        self._chromatic_dispersion = chromatic_dispersion
+
+    @property
+    def pmd(self):
+        return self._pmd
+
+    @pmd.setter
+    def pmd(self, pmd):
+        self._pmd = pmd
+
+    @property
+    def channel_number(self):
+        return self._channel_number
+
+    @property
+    def carriers(self):
+        entries = zip(self.channel_number, self.frequency, self.baud_rate,
+                      self.roll_off, self.powers, self.chromatic_dispersion, self.pmd)
+        return [Channel(*entry) for entry in entries]
+
+    def __add__(self, si):
+        frequency = append(self.frequency, si.frequency)
+        baud_rate = append(self.baud_rate, si.baud_rate)
+        slot_width = append(self.slot_width, si.slot_width)
+        signal = append(self.signal, si.signal)
+        nli = append(self.nli, si.nli)
+        ase = append(self.ase, si.ase)
+        roll_off = append(self.roll_off, si.roll_off)
+        chromatic_dispersion = append(self.chromatic_dispersion, si.chromatic_dispersion)
+        pmd = append(self.pmd, si.pmd)
+        number_of_channels = self.number_of_channels + si.number_of_channels
+        power = lin2db(mean(signal) * 1e3)
+        pref = Pref(power, power, lin2db(number_of_channels))
+        try:
+            si = SpectralInformation(frequency=frequency, slot_width=slot_width,
+                                     signal=signal, nli=nli, ase=ase,
+                                     baud_rate=baud_rate, roll_off=roll_off,
+                                     chromatic_dispersion=chromatic_dispersion, pmd=pmd)
+        except InfoError:
+            raise InfoError('Spectra cannot be summed: channels overlapping.')
+        return si
+
+    def _replace(self, carriers, pref):
+        self.chromatic_dispersion = array([c.chromatic_dispersion for c in carriers])
+        self.pmd = array([c.pmd for c in carriers])
+        self.signal = array([c.power.signal for c in carriers])
+        self.nli = array([c.power.nli for c in carriers])
+        self.ase = array([c.power.ase for c in carriers])
+        self.pref = pref
+        return self
+
+
+def dimension_reshape(value, dimension, default=None, name=None):
+    if value is None:
+        if default is not None:
+            value = dimension_reshape(value=default, dimension=dimension, name=name)
+        else:
+            raise InfoError(f'Missing mandatory field: {name}.')
+    elif not isinstance(value, Sized):
+        value = value * ones(dimension)
+    else:
+        if len(value) == 1:
+            value = value[0] * ones(dimension)
+        elif len(value) == dimension:
+            value = squeeze(value)
+        else:
+            raise InfoError(f'Dimension mismatch field: {name}.')
+    return value
+
+
+def create_arbitrary_spectral_information(frequency, slot_width=None, signal=None, baud_rate=None,
+                                          roll_off=None, chromatic_dispersion=None, pmd=None):
+    """ Creates an arbitrary spectral information """
+    if isinstance(frequency, Sized):
+        frequency = squeeze(frequency)
+        number_of_channels = len(frequency)
+    else:
+        frequency = array([frequency])
+        number_of_channels = 1
+    baud_rate = dimension_reshape(baud_rate, number_of_channels, None, 'baud rate')
+    slot_width = dimension_reshape(slot_width, number_of_channels, ceil(baud_rate / BASE_SLOT_WIDTH) * BASE_SLOT_WIDTH,
+                                   'slot_width')
+    signal = dimension_reshape(signal, number_of_channels, 0, 'signal')
+    nli = zeros(number_of_channels)
+    ase = zeros(number_of_channels)
+    roll_off = dimension_reshape(roll_off, number_of_channels, 0, 'roll_off')
+    chromatic_dispersion = dimension_reshape(chromatic_dispersion, number_of_channels, 0, 'chromatic dispersion')
+    pmd = dimension_reshape(pmd, number_of_channels, 0, 'pmd')
+    si = SpectralInformation(frequency=frequency, slot_width=slot_width,
+                             signal=signal, nli=nli, ase=ase,
+                             baud_rate=baud_rate, roll_off=roll_off,
+                             chromatic_dispersion=chromatic_dispersion, pmd=pmd)
+    return si
 
 
 def create_input_spectral_information(f_min, f_max, roll_off, baud_rate, power, spacing):
-    # pref in dB : convert power lin into power in dB
-    pref = lin2db(power * 1e3)
+    """ Creates a fixed slot width spectral information with flat power """
     nb_channel = automatic_nch(f_min, f_max, spacing)
-    si = SpectralInformation(
-        pref=Pref(pref, pref, lin2db(nb_channel)),
-        carriers=[
-            Channel(f, (f_min + spacing * f),
-                    baud_rate, roll_off, Power(power, 0, 0), 0, 0) for f in range(1, nb_channel + 1)
-        ]
+    frequency = [(f_min + spacing * i) for i in range(1, nb_channel + 1)]
+    si = create_arbitrary_spectral_information(
+        frequency, slot_width=spacing, signal=power, baud_rate=baud_rate, roll_off=roll_off
     )
     return si
---
+++ tests/test_info.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import pytest
+from numpy import array, zeros, ones
+from numpy.testing import assert_array_equal
+
+from gnpy.core.info import dimension_reshape, create_arbitrary_spectral_information
+from gnpy.core.exceptions import InfoError
+
+
+def test_dim_reshape():
+    assert_array_equal(dimension_reshape(value=[1, 2, 3], dimension=3), array([1, 2, 3]))
+    assert_array_equal(dimension_reshape(value=[1], dimension=3), array([1, 1, 1]))
+    assert_array_equal(dimension_reshape(value=1, dimension=1), array([1]))
+    assert_array_equal(dimension_reshape(value=1, dimension=3), array([1, 1, 1]))
+    assert_array_equal(dimension_reshape(value=None, dimension=2, default=[1, 2]), array([1, 2]))
+    assert_array_equal(dimension_reshape(value=None, dimension=3, default=1), array([1, 1, 1]))
+    with pytest.raises(InfoError) as e:
+        dimension_reshape(value=[1, 2, 3], dimension=1, name='field_name')
+    assert str(e.value) == 'Dimension mismatch field: field_name.'
+    with pytest.raises(InfoError) as e:
+        dimension_reshape(value=None, dimension=3, default=None, name='field_name')
+    assert str(e.value) == 'Missing mandatory field: field_name.'
+
+
+def test_create_arbitrary_spectral_information():
+    frequency = [193.25e12, 193.3e12, 193.35e12]
+    baud_rate = 32e9
+    signal = [1, 1, 1]
+    si = create_arbitrary_spectral_information(frequency=frequency, baud_rate=baud_rate, signal=signal)
+    assert_array_equal(si.baud_rate, array([32e9, 32e9, 32e9]))
+    assert_array_equal(si.slot_width, array([37.5e9, 37.5e9, 37.5e9]))
+    assert_array_equal(si.signal, ones(3))
+    assert_array_equal(si.nli, zeros(3))
+    assert_array_equal(si.ase, zeros(3))
+    assert_array_equal(si.roll_off, zeros(3))
+    assert_array_equal(si.chromatic_dispersion, zeros(3))
+    assert_array_equal(si.pmd, zeros(3))
+    assert_array_equal(si.channel_number, array([1, 2, 3]))
+    assert_array_equal(si.number_of_channels, 3)
+
+    with pytest.raises(InfoError) as e:
+        si += si
+    assert str(e.value) == 'Spectra cannot be summed: channels overlapping.'
+
+    frequency = [193.35e12, 193.3e12, 193.25e12]
+    signal = [1, 2, 3]
+    si = create_arbitrary_spectral_information(frequency=frequency, baud_rate=baud_rate, signal=signal)
+    assert_array_equal(si.signal, array([3, 2, 1]))
+
+    frequency = [193.25e12, 193.3e12, 193.35e12]
+    baud_rate = 64e9
+    slot_width = 50e9
+    with pytest.raises(InfoError) as e:
+        create_arbitrary_spectral_information(frequency=frequency, baud_rate=baud_rate, slot_width=slot_width)
+    assert str(e.value) == 'Spectrum baud rate larger than the slot width.'
+
+    frequency = [193.26e12, 193.3e12, 193.34e12]
+    baud_rate = 40e9
+    with pytest.raises(InfoError) as e:
+        create_arbitrary_spectral_information(frequency=frequency, baud_rate=baud_rate)
+    assert str(e.value) == 'Spectrum required slot widths larger than the frequency spectral distances.'
