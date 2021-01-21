9276727b9fc865b5df0c842c4e8030fd7e21560a
Giacomo Borraccini <giacomo.borraccini@polito.it>
2021-01-21 18:44:00 +0100

    Management of lumped losses along a fiber span
    Change-Id: I73a6baa321aca4d041cafa180f47afed824ce267

--- tests/test_science_utils.py
+++ tests/test_science_utils.py
@@ -6,14 +6,17 @@ Checks that RamanFiber propagates properly the spectral information. In this way
 are tested.
 """
 
+import pytest
 from pathlib import Path
 from pandas import read_csv
 from numpy.testing import assert_allclose
-from numpy import array
+from numpy import array, load
 
 from gnpy.core.info import create_input_spectral_information, create_arbitrary_spectral_information
 from gnpy.core.elements import Fiber, RamanFiber
 from gnpy.tools.json_io import load_json
+from gnpy.core.exceptions import NetworkTopologyError
+from gnpy.core.science_utils import RamanSolver
 
 from tests.test_parameters import MockSimParams, set_sim_params
 
@@ -75,3 +78,49 @@ def test_raman_fiber(set_sim_params):
     assert_allclose(p_signal, expected_results['signal'], rtol=1e-3)
     assert_allclose(p_ase, expected_results['ase'], rtol=1e-3)
     assert_allclose(p_nli, expected_results['nli'], rtol=1e-3)
+
+
+@pytest.mark.parametrize("loss, position, errmsg", (
+        (0.5, -2, f"Lumped loss positions must be between 0 and the fiber length (80.0 km), boundaries excluded."),
+        (0.5, 81, f"Lumped loss positions must be between 0 and the fiber length (80.0 km), boundaries excluded.")))
+def test_fiber_lumped_losses(loss, position, errmsg, set_sim_params):
+    """ Check on Fiber with lumped losses."""
+    MockSimParams.set_params(load_json(TEST_DIR / 'data' / 'sim_params.json'))
+    fiber_dict = load_json(TEST_DIR / 'data' / 'test_lumped_losses_fiber_config.json')
+    fiber_dict['params']['lumped_losses'] = [{'position': position, 'loss': loss}]
+    with pytest.raises(NetworkTopologyError) as e:
+        Fiber(**fiber_dict)
+    assert str(e.value) == errmsg
+
+
+def test_fiber_lumped_losses_srs(set_sim_params):
+    """ Test the accuracy of Fiber with lumped losses propagation."""
+    # spectral information generation
+    spectral_info_input = create_input_spectral_information(f_min=191.3e12, f_max=196.1e12, roll_off=0.15,
+                                                            baud_rate=32e9, power=1e-3, spacing=50e9)
+
+    MockSimParams.set_params(load_json(TEST_DIR / 'data' / 'sim_params.json'))
+    sim_params = MockSimParams.get()
+    fiber = Fiber(**load_json(TEST_DIR / 'data' / 'test_lumped_losses_fiber_config.json'))
+    raman_fiber = RamanFiber(**load_json(TEST_DIR / 'data' / 'test_lumped_losses_raman_fiber_config.json'))
+
+    # propagation
+    # without Raman pumps
+    expected_power_profile = load(TEST_DIR / 'data' / 'test_lumped_losses_fiber_no_pumps.npy')
+    stimulated_raman_scattering = RamanSolver.calculate_stimulated_raman_scattering(
+        spectral_info_input, fiber)
+    power_profile = stimulated_raman_scattering.power_profile
+    assert_allclose(power_profile, expected_power_profile, rtol=1e-3)
+
+    # with Raman pumps
+    expected_power_profile = load(TEST_DIR / 'data' / 'test_lumped_losses_raman_fiber.npy')
+    stimulated_raman_scattering = RamanSolver.calculate_stimulated_raman_scattering(
+        spectral_info_input, raman_fiber)
+    power_profile = stimulated_raman_scattering.power_profile
+    assert_allclose(power_profile, expected_power_profile, rtol=1e-3)
+
+    # without Stimulated Raman Scattering
+    expected_power_profile = load(TEST_DIR / 'data' / 'test_lumped_losses_fiber_no_raman.npy')
+    stimulated_raman_scattering = RamanSolver.calculate_attenuation_profile(spectral_info_input, fiber)
+    power_profile = stimulated_raman_scattering.power_profile
+    assert_allclose(power_profile, expected_power_profile, rtol=1e-3)
