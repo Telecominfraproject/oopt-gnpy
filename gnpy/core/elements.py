4ad1288bacfbe82bdd51408a6bbe8e77bb8be4dd
EstherLerouzic <esther.lerouzic@orange.com>
2021-04-28 10:03:24 +0200

    address comment from Andrea on the computation of span los also for fibers
    use loss computed based on input output total power instead of theaoretical loss based
    on loss coef and con in con out. this way, we can catch the effect of slope.
    
    Signed-off-by: EstherLerouzic <esther.lerouzic@orange.com>
    Change-Id: I762ec13f54c65cf1065fb9de6a9dd03d8679356a

--- gnpy/core/elements.py
+++ gnpy/core/elements.py
@@ -517,9 +517,10 @@ class Fiber(_Node):
         spectral_info.ase *= attenuation_out
 
     def update_pref(self, spectral_info):
+        loss = self._pch_in - round(lin2db(mean(spectral_info.signal) * 1e3), 2)
         self.pch_out_db = round(lin2db(mean(spectral_info.signal) * 1e3), 2)
         spectral_info.pref = spectral_info.pref._replace(p_span0=spectral_info.pref.p_span0,
-                                                         p_spani=spectral_info.pref.p_spani - self.loss)
+                                                         p_spani=spectral_info.pref.p_spani - loss)
 
     def __call__(self, spectral_info):
         self._pch_in = round(lin2db(mean(spectral_info.signal) * 1e3), 2)
@@ -581,12 +582,6 @@ class RamanFiber(Fiber):
         spectral_info.signal *= attenuation_out
         spectral_info.nli *= attenuation_out
         spectral_info.ase *= attenuation_out
-
-    def update_pref(self, spectral_info):
-        loss = self._pch_in - round(lin2db(mean(spectral_info.signal) * 1e3), 2)
-        self.pch_out_db = round(lin2db(mean(spectral_info.signal) * 1e3), 2)
-        spectral_info.pref = spectral_info.pref._replace(p_span0=spectral_info.pref.p_span0,
-                                                         p_spani=spectral_info.pref.p_spani - loss)
 
 
 class Edfa(_Node):
