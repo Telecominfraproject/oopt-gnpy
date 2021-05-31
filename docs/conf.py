5ff71825b8e0d9b294756c74341eb7516389f922
Jan Kundrát <jan.kundrat@telecominfraproject.com>
2021-06-01 01:23:52 +0200

    docs: allow writing Sphinx docs in Markdown
    ...because it's less annoying than the ReStructuredText.
    
    Change-Id: Ic3db1fe1834580b0f31190b3bf96d6ab95c3b40e

--- docs/conf.py
+++ docs/conf.py
@@ -36,7 +36,13 @@ extensions = ['sphinx.ext.autodoc',
               'sphinxcontrib.bibtex',
               'pbr.sphinxext',
               'sphinx.ext.graphviz',
+              'myst_parser',
               ]
+
+myst_enable_extensions = [
+    "deflist",
+    "dollarmath",
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
