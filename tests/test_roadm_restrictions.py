f0a16b731c112eabb8bb35ff5a0e196ff275b6bf
EstherLerouzic <esther.lerouzic@orange.com>
2021-07-29 18:13:10 +0200

    Check all element attributes before and after propagation
    The test checks behaviour for power saturation and for many
    combination of design power and propagation power.
    
    Signed-off-by: EstherLerouzic <esther.lerouzic@orange.com>
    Change-Id: I2fec00232c80dd395e4dec20ec531c9c2e127760

--- tests/test_roadm_restrictions.py
+++ tests/test_roadm_restrictions.py
@@ -11,15 +11,18 @@ checks that restrictions in roadms are correctly applied during autodesign
 """
 
 from pathlib import Path
+from copy import deepcopy
 import pytest
 from gnpy.core.utils import lin2db, automatic_nch
-from gnpy.core.elements import Fused, Roadm, Edfa
+from gnpy.core.elements import Fused, Roadm, Edfa, Transceiver, EdfaOperational, EdfaParams, Fiber
+from gnpy.core.parameters import FiberParams
 from gnpy.core.network import build_network
 from gnpy.tools.json_io import network_from_json, load_equipment, load_json, Amp
 from gnpy.core.equipment import trx_mode_params
-from gnpy.topology.request import PathRequest, compute_constrained_path
+from gnpy.topology.request import PathRequest, compute_constrained_path, propagate
 from gnpy.core.info import create_input_spectral_information
 from gnpy.core.utils import db2lin
+
 
 TEST_DIR = Path(__file__).parent
 EQPT_LIBRARY_NAME = TEST_DIR / 'data/eqpt_config.json'
@@ -286,3 +289,280 @@ def test_roadm_target_power(prev_node_type, effective_pch_out_db, power_dbm):
                         assert power == pytest.approx(carriers_power_in_roadm, rel=1e-3)
         else:
             si = el(si)
+
+
+def create_per_oms_request(network, eqpt, req_power):
+    """ create request template
+    """
+    params = {'trx_type': '',
+              'trx_mode': '',
+              'bidir': False,
+              'loose_list': ['strict', 'strict'],
+              'format': '',
+              'path_bandwidth': 100e9}
+    trx_params = trx_mode_params(eqpt)
+    params.update(trx_params)
+    trxs = [e for e in network if isinstance(e, Transceiver)]
+    req_list = []
+    req_id = 0
+    for trx in trxs:
+        source = trx.uid
+        roadm = next(n for n in network.successors(trx) if isinstance(n, Roadm))
+        for degree in roadm.per_degree_pch_out_db.keys():
+            node = next(n for n in network.nodes() if n.uid == degree)
+            # find next roadm
+            while not isinstance(node, Roadm):
+                node = next(n for n in network.successors(node))
+            next_roadm = node
+            destination = next(n.uid for n in network.successors(next_roadm) if isinstance(n, Transceiver))
+            params['request_id'] = req_id
+            req_id += 1
+            params['source'] = source
+            params['destination'] = destination
+            params['nodes_list'] = [degree, destination]
+            req = PathRequest(**params)
+            req.power = db2lin(req_power - 30)
+            carrier = {key: getattr(req, key) for key in ['power', 'baud_rate', 'roll_off']}
+            req.initial_spectrum = {(req.f_min + req.spacing * f):
+                                    deepcopy(carrier) for f in range(1, req.nb_channel + 1)}
+            req_list.append(req)
+    # add one additional request crossing several roadms to have a complete view
+    params['source'] = 'trx Rennes_STA'
+    params['destination'] = 'trx Vannes_KBE'
+    params['nodes_list'] = ['roadm Lannion_CAS', 'trx Vannes_KBE']
+    params['bidir'] = True
+    req = PathRequest(**params)
+    req.power = db2lin(req_power - 30)
+    carrier = {key: getattr(req, key) for key in ['power', 'baud_rate', 'roll_off']}
+    req.initial_spectrum = {(req.f_min + req.spacing * f): deepcopy(carrier) for f in range(1, req.nb_channel + 1)}
+    req_list.append(req)
+    return req_list
+
+
+def list_element_attr(element):
+    """ give the list of keys to be checked depending on element type. List only the keys that are not
+    created upon element effective propagation
+    TODO: many parameters (location ones) look redondant: simplify ?
+    """
+    if isinstance(element, Roadm):
+        return ['effective_pch_out_db', 'lat', 'latitude', 'lng', 'loc',
+                'location', 'longitude', 'loss', 'metadata', 'name', 'operational', 'params', 'passive',
+                'per_degree_pch_out_db', 'restrictions', 'type_variety']
+        # dynamically created: 'effective_loss',
+        # TODO: loss is not updated at all : dead param ?
+    if isinstance(element, Edfa):
+        return ['delta_p', 'effective_gain',
+                'lat', 'latitude', 'lng', 'loc', 'location', 'longitude', 'metadata', 'name',
+                'operational', 'out_voa', 'params', 'passive',
+                'tilt_target']
+        # TODO this exhaustive test highlighted that type_variety is not correctly updated from EdfaParams to
+        # attributes in preamps
+        # dynamically created only with channel propagation: 'att_in', 'channel_freq', 'effective_pch_out_db'
+        # 'gprofile', 'interpol_dgt', 'interpol_gain_ripple', 'interpol_nf_ripple', 'nch',  'nf', 'pin_db', 'pout_db',
+        # 'target_pch_out_db',
+    if isinstance(element, EdfaOperational):
+        return ['delta_p', 'gain_target', 'out_voa', 'tilt_target']
+    if isinstance(element, EdfaParams):
+        return ['allowed_for_design', 'dgt', 'dual_stage_model', 'f_max', 'f_min', 'gain_flatmax', 'gain_min',
+                'gain_ripple', 'nf_fit_coeff', 'nf_model', 'nf_ripple', 'out_voa_auto', 'p_max', 'raman',
+                'type_def', 'type_variety']
+    if isinstance(element, Fiber):
+        return ['lat', 'latitude', 'lng', 'loc', 'location',
+                'longitude', 'loss', 'metadata', 'name', 'operational',
+                'params', 'passive', 'pmd', 'type_variety']
+        # dynamically created 'output_total_power', 'pch_out_db'
+    if isinstance(element, FiberParams):
+        return ['att_in', 'beta2', 'beta3', 'con_in', 'con_out', 'dispersion', 'dispersion_slope', 'f_loss_ref',
+                'gamma', 'length', 'loss_coef', 'pmd_coef', 'pumps_loss_coef', 'raman_efficiency', 'ref_frequency',
+                'ref_wavelength']
+    if isinstance(element, Fused):
+        return ['lat', 'latitude', 'lng', 'loc', 'location', 'longitude', 'loss', 'metadata', 'name',
+                'operational', 'params', 'passive']
+
+    raise ValueError('This type is not a supported element')
+
+
+def verify_first_amp(base_amps, element_after_propagation, power_dbm, req_power, dp, pch_max):
+    """ returns power difference after-before propagation according to the different cases:
+    if first amp has a type_variety attribute, design does not apply a saturation verification on amplifiers
+    and computed power out before propagation is power_dBm + dp
+    else element was saturated (power = min(pch_max, power_dbm + dp)).
+    in any case, saturation is applied during propagation, knowing that if
+    design was saturated then its delta_p was changed with the required power reduction.
+    I used the power to compute the expected gain of each amp wrt each cases.
+    """
+    if hasattr(base_amps[element_after_propagation.uid], 'type_variety'):
+        # no saturation verification during design
+        # case CC
+        return min(pch_max, req_power + dp) - power_dbm - dp
+    # case DD
+    # saturation verification during design
+    return min(pch_max, req_power + element_after_propagation.delta_p) - min(pch_max, power_dbm + dp)
+
+
+def verify_other_amps(base_amps, element_after_propagation, previous_amp_was_saturated, power_dbm, req_power,
+                      previous_deltap, pch_max):
+    """ returns power difference after-before propagation according to the different cases:
+    all combinations of saturation for previous amp and this amp are:
+                 |                              this amp
+    --------------------------------------------------------------------------
+                 |               saturated if over pchmax   | not saturated
+                 | saturated if |
+    previous amp | over pchmax  |          FFF              |     EEE
+                 | -----------------------------------------------------------
+                 | not saturate |          HHH              |     GGG
+
+    I used the power to compute the expected gain of each amp wrt each cases, knowing that if
+    design was saturated then its delta_p was changed with the required power reduction
+    """
+    if hasattr(base_amps[element_after_propagation.uid], 'type_variety'):  # current amp not saturated
+        if previous_amp_was_saturated:
+            # case EEE
+            # previous amp was possibly saturated during design and this one was not
+            return min(pch_max, req_power + element_after_propagation.delta_p) -\
+                   min(pch_max, req_power + previous_deltap) -\
+                   (power_dbm + element_after_propagation.delta_p) +\
+                   min(pch_max, power_dbm + previous_deltap)
+        # case GGG
+        return min(pch_max, req_power + element_after_propagation.delta_p) -\
+               min(pch_max, req_power + previous_deltap) -\
+               (power_dbm + element_after_propagation.delta_p) +\
+               (power_dbm + previous_deltap)
+    else:   # current amp not saturated
+        if previous_amp_was_saturated:
+            # case FFF
+            return min(pch_max, req_power + element_after_propagation.delta_p) -\
+                   min(pch_max, req_power + previous_deltap) -\
+                   min(pch_max, power_dbm + element_after_propagation.delta_p) +\
+                   min(pch_max, power_dbm + previous_deltap)
+        # case HHH
+        return min(pch_max, req_power + element_after_propagation.delta_p) -\
+               min(pch_max, req_power + previous_deltap) -\
+               min(pch_max, power_dbm + element_after_propagation.delta_p) +\
+               power_dbm + previous_deltap
+
+
+# all initial delta_p are null in topo file, so add random places to change this value
+@pytest.mark.parametrize('amps_with_adjusted_delta_p', [[],
+                         ['east edfa in Lorient_KMA to Vannes_KBE',
+                          'east edfa in Stbrieuc to Rennes_STA',
+                          'west edfa in Lannion_CAS to Morlaix',
+                          'east edfa in a to b',
+                          'west edfa in b to a']])
+@pytest.mark.parametrize('power_dbm, req_power', [(0, 0), (0, -3), (3, 3), (0, 3), (3, 0),
+                                                  (3, 1), (3, 5), (3, 2), (3, 4), (2, 4)])
+def test_booster_target_power_and_gain(power_dbm, req_power, amps_with_adjusted_delta_p):
+    """ Check that network design does not change after propagation
+    except for gain in case of power_saturation during design and/or during propagation:
+    - in power mode only:
+        expected behaviour: target power out of roadm does not change
+        so gain of booster should be reduced/augmented by the exact power difference
+        the rest of the amplifier have unchanged gain
+        except if augmentation leads to total_power above amplifier max power
+         ie if amplifier saturates. then first amplifier in OMS is impacted
+         eg
+                                roadm -----booster (pmax 21dBm, 96 channels= 19.82dB)
+        pdesign=0dBm pch= 0dBm,         ^ -20dBm  ^G=20dB, Pch=0dBm, Ptot=19.82dBm
+        pdesign=0dBm pch= -3dBm         ^ -20dBm  ^G=17dB, Pch=-3dBm, Ptot=16.82dBm
+        pdesign=3dBm pch= 3dBm          ^ -20dBm  ^G=23-1.82dB, Pch=1.18dBm, Ptot=21dBm
+            amplifier can not handle 96x3dBm channels, amplifier saturation is considered for the choice
+            of amplifier if no type variety has been provided, else the saturation is applied only upon propagation
+        pdesign=0dBm pch= 3dBm          ^ -20dBm  ^G=23-1.82dB, Pch=1.18dBm, Ptot=21dBm
+            amplifier can not handle 96x3dBm channels, amplifier selection has been done for 0dBm
+            saturation is applied for all amps only during propagation
+        design applies a saturation verification on amplifiers only if no type_variety is defined for it.
+        this saturation leads to a power reduction to the max power in the amp library, which is also applied on
+        the amp delta_p.
+        This saturation occurs per amplifier and independantly from propagation.
+        After design, upon propagation, the amplifier gain may change due to different total power used than
+        during design (eg not the same nb of channels, not the same power per channel).
+        This test also checks all the possible combinations and expected before/after propagation gain differences.
+        it also checks delta_p is applied due to saturation during design
+    """
+    eqpt = load_equipment(EQPT_LIBRARY_NAME)
+    eqpt['SI']['default'].power_dbm = power_dbm
+    json_network = load_json(NETWORK_FILE_NAME)
+    for element in json_network['elements']:
+        for name in amps_with_adjusted_delta_p:
+            if element['uid'] == name:
+                element['operational']['delta_p'] = 1
+
+    network = network_from_json(json_network, eqpt)
+    # Build the network once using the default power defined in SI in eqpt config
+    p_db = power_dbm
+    p_total_db = p_db + lin2db(automatic_nch(eqpt['SI']['default'].f_min,
+                                             eqpt['SI']['default'].f_max,
+                                             eqpt['SI']['default'].spacing))
+    base_network = deepcopy(network)
+    base_amps = {amp.uid: amp for amp in base_network.nodes() if isinstance(amp, Edfa)}
+    build_network(network, eqpt, p_db, p_total_db)
+    # record network settings before propagating
+    network_copy = deepcopy(network)
+    # propagate on each oms
+    req_list = create_per_oms_request(network, eqpt, req_power)
+    paths = [compute_constrained_path(network, r) for r in req_list]
+    _ = [propagate(p, r, eqpt) for p, r in zip(paths, req_list)]
+
+    # systematic comparison of elements settings before and after propagation
+    # all amps have 21 dBm max power
+    pch_max = 21 - lin2db(96)
+    for pth in paths:
+        # check all elements except source and destination trx
+        for el in pth:
+            print(el)
+        previous_deltap = None
+        for i, element in enumerate(pth[1:-1]):
+            # index of previous element in path is i
+            element_is_first_amp = isinstance(element, Edfa) and isinstance(pth[i], Roadm)
+
+            previous_amp_was_saturated = ((isinstance(element, Edfa) and isinstance(pth[i], Roadm))
+                                          and not hasattr(base_amps[element.uid], 'type_variety'))
+            if element.uid == 'west edfa in d to c':
+                # OMS c to d has no booster but one preamp: the power difference is held there
+                element_is_first_amp = True
+            element_copy = next(n for n in network_copy.nodes() if n.uid == element.uid)
+            for key in list_element_attr(element):
+                parameters = getattr(element, key)
+                parameters_copy = getattr(element_copy, key)
+                print(element.uid, key, parameters, parameters_copy)
+                if not isinstance(parameters, (EdfaOperational, EdfaParams, FiberParams)):
+                    if not key == 'effective_gain':
+                        assert parameters == parameters_copy
+                    else:
+                        dp = 0 if element.uid not in amps_with_adjusted_delta_p else 1
+                        if element_is_first_amp:
+                            # if first amp has a type_variety attribute, design does not apply a saturation
+                            assert element.effective_gain - element_copy.effective_gain ==\
+                                pytest.approx(verify_first_amp(base_amps, element, power_dbm,
+                                                               req_power, dp, pch_max), abs=1e-2)
+                        else:
+                            # then all combinations of saturation for previous amp and this amp:
+                            #              |                              this amp
+                            # --------------------------------------------------------------------------
+                            #              |               saturated if over pchmax   | not saturated
+                            #              | saturated if |
+                            # previous amp | over pchmax  |          FFF              |     EEE
+                            #              | -----------------------------------------------------------
+                            #              | not saturate |          HHH              |     GGG
+                            #
+                            # I used the power to compute the expected gain of each amp wrt each cases, knowing that if
+                            # design was saturated then its delta_p was changed with the required power reduction
+
+                            assert element.effective_gain - element_copy.effective_gain ==\
+                                        pytest.approx(verify_other_amps(base_amps, element, previous_amp_was_saturated,
+                                            power_dbm, req_power, previous_deltap, pch_max), abs=1e-2)
+                        # if amp has no type_variety then its output total power is computed to choose a type_variety
+                        # in the library, and if all amps have max_power below this vaue, a reduction of the target
+                        # power is applied on delta_p. This test checks that the reduction is correctly computed
+                        previous_deltap = element.delta_p
+                        if hasattr(base_amps[element.uid], 'type_variety'):
+                            # case AA
+                            previous_amp_was_saturated = False
+                            assert element.delta_p == dp
+                        else:
+                            # case BB
+                            previous_amp_was_saturated = True
+                            assert element.delta_p == pytest.approx(min(power_dbm + dp, pch_max) - power_dbm, abs=1e-2)
+                else:
+                    for one_param in list_element_attr(parameters):
+                        assert getattr(parameters, one_param) == getattr(parameters_copy, one_param)
