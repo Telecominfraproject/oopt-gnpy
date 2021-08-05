bb2707ce2bc45f9908c9770f131692e99f7defee
EstherLerouzic <esther.lerouzic@orange.com>
2021-08-05 15:04:58 +0200

    Change pref from a scalar to a list of per channel delta power
    The idea behind this change is to reproduce the exact same behaviour as
    with the scalar, but accounting for variable levels of powers.
    
    - delete the  neq_ch: equivalent channel count in dB because with mixed
      rates and power such a value has limited utility
    - instead creates a vector that records the 'user defined' distribution
      of power.
    
    This vector is used as a reference for channel equalization out of the
    ROADM. If target_power_per_channel has some channels power above
    input power, then the whole target is reduced.
    For example, if user specifies delta_pdb_per_channel:
       freq1: 1dB, freq2: 3dB, freq3: -3dB, and target is -20dBm out of the ROADM,
    then the target power for each channel uses the specified
    delta_pdb_per_channel.
       target_power_per_channel[f1, f2, f3] = -19, -17, -23
    However if input_signal = -23, -16, -26, then the target can not be applied,
    because -23 < -19dBm and -26 < -23dBm, and a reduction must be applied
    (ROADM can not amplify).
    In order to keep the same relative power difference as specified by
    delta_pdb_per_channel, on channels out of the ROADM, the target is reduced by
    4 dB and the new target is [-23, -21, -27]
    
    - in amplifier: the saturation is computed based on this vector
    delta_pdb_per_channel, instead of the nb of channels.
    The target of the future refactor will be to use the effective
    carrier's power. I prefer to have this first step, because this is
    how it is implemented today (ie based on the noiseless reference),
    and I would like first to add more behaviour tests before doing
    this refactor (would it be needed).
    
    - in spectralInfo class, change pref to a Pref object to enable both
    p_span0 and p_span0 to be convey during propagation of spectral_information
    in elements spectral
    
    Signed-off-by: EstherLerouzic <esther.lerouzic@orange.com>
    Change-Id: I591027cdd08e89098330c7d77d6f50212f4d4724

--- tests/test_info.py
+++ tests/test_info.py
@@ -4,19 +4,21 @@
 import pytest
 from numpy import array, zeros, ones
 from numpy.testing import assert_array_equal
-
-from gnpy.core.info import create_arbitrary_spectral_information
+from gnpy.core.info import create_arbitrary_spectral_information, Pref
 from gnpy.core.exceptions import SpectrumError
 
 
 def test_create_arbitrary_spectral_information():
     si = create_arbitrary_spectral_information(frequency=[193.25e12, 193.3e12, 193.35e12],
-                                               baud_rate=32e9, signal=[1, 1, 1])
+                                               baud_rate=32e9, signal=[1, 1, 1],
+                                               delta_pdb_per_channel=[1, 1, 1],
+                                               ref_power=Pref(1, 1))
     assert_array_equal(si.baud_rate, array([32e9, 32e9, 32e9]))
     assert_array_equal(si.slot_width, array([37.5e9, 37.5e9, 37.5e9]))
     assert_array_equal(si.signal, ones(3))
     assert_array_equal(si.nli, zeros(3))
     assert_array_equal(si.ase, zeros(3))
+    assert_array_equal(si.delta_pdb_per_channel, ones(3))
     assert_array_equal(si.roll_off, zeros(3))
     assert_array_equal(si.chromatic_dispersion, zeros(3))
     assert_array_equal(si.pmd, zeros(3))
@@ -29,22 +31,25 @@ def test_create_arbitrary_spectral_information():
 
     si = create_arbitrary_spectral_information(frequency=array([193.35e12, 193.3e12, 193.25e12]),
                                                slot_width=array([50e9, 50e9, 50e9]),
-                                               baud_rate=32e9, signal=array([1, 2, 3]))
+                                               baud_rate=32e9, signal=array([1, 2, 3]),
+                                               ref_power=Pref(1, 1))
     assert_array_equal(si.signal, array([3, 2, 1]))
 
     with pytest.raises(SpectrumError, match='Spectrum baud rate, including the roll off, '
                                             r'larger than the slot width for channels: \[1, 3\].'):
         create_arbitrary_spectral_information(frequency=[193.25e12, 193.3e12, 193.35e12], signal=1,
-                                              baud_rate=[64e9, 32e9, 64e9], slot_width=50e9)
+                                              baud_rate=[64e9, 32e9, 64e9], slot_width=50e9,
+                                              ref_power=Pref(1, 1))
     with pytest.raises(SpectrumError, match='Spectrum required slot widths larger than the frequency spectral '
                                             r'distances between channels: \[\(1, 2\), \(3, 4\)\].'):
         create_arbitrary_spectral_information(frequency=[193.26e12, 193.3e12, 193.35e12, 193.39e12], signal=1,
-                                              baud_rate=32e9, slot_width=50e9)
+                                              baud_rate=32e9, slot_width=50e9, ref_power=Pref(1, 1))
     with pytest.raises(SpectrumError, match='Spectrum required slot widths larger than the frequency spectral '
                                             r'distances between channels: \[\(1, 2\), \(2, 3\)\].'):
         create_arbitrary_spectral_information(frequency=[193.25e12, 193.3e12, 193.35e12], signal=1, baud_rate=49e9,
-                                              roll_off=0.1)
+                                              roll_off=0.1, ref_power=Pref(1, 1))
 
     with pytest.raises(SpectrumError,
                        match='Dimension mismatch in input fields.'):
-        create_arbitrary_spectral_information(frequency=[193.25e12, 193.3e12, 193.35e12], signal=[1, 2], baud_rate=49e9)
+        create_arbitrary_spectral_information(frequency=[193.25e12, 193.3e12, 193.35e12], signal=[1, 2], baud_rate=49e9,
+                                              ref_power=Pref(1, 1))
