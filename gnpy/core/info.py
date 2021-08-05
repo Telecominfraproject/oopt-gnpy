6fcd34698fe97ff57391d0666936d8a0c70773f4
EstherLerouzic <esther.lerouzic@orange.com>
2021-08-05 15:07:35 +0200

    Add a user defined initial spectrum in propagation functions
    A new function is added to build spectrum information based on
    the actual mixture of channels to be simulated (baud rate, slot width,
    power per frequency).
    
    Propagation functions are changed so that, if the user defines a
    specific distribution, then they use it, else they use as before,
    all identical channels based on the initial request. In this case,
    as before this change, we assume full load, with same channel for
    the spectral info and not the resulting mixt of channels after
    routing.
    
    Signed-off-by: EstherLerouzic <esther.lerouzic@orange.com>
    Change-Id: Icf56396837b77009e98accd27fcebd2dded6d112

--- gnpy/core/info.py
+++ gnpy/core/info.py
@@ -292,3 +292,21 @@ def create_input_spectral_information(f_min, f_max, roll_off, baud_rate, power, 
     return create_arbitrary_spectral_information(frequency, slot_width=spacing, signal=power, baud_rate=baud_rate,
                                                  roll_off=roll_off, delta_pdb_per_channel=delta_pdb_per_channel,
                                                  ref_power=Pref(p_span0=p_span0, p_spani=p_spani))
+
+
+def use_initial_spectrum(initial_spectrum, ref_carrier):
+    """ initial spectrum is a dict with key = carrier frequency, and value a dict with power,
+    baudrate and roll off for this carrier. ref_power is a Pref object with the power used for the reference channel
+    """
+    frequency = list(initial_spectrum.keys())
+    signal = [ref_carrier['req_power'] * db2lin(s['delta_pdb']) for s in initial_spectrum.values()]
+    roll_off = [s['roll_off'] for s in initial_spectrum.values()]
+    baud_rate = [s['baud_rate'] for s in initial_spectrum.values()]
+    delta_pdb_per_channel = array([s['delta_pdb'] for s in initial_spectrum.values()])
+    slot_width = [s['slot_width'] for s in initial_spectrum.values()]
+    p_span0 = watt2dbm(ref_carrier['req_power'])
+    p_spani = watt2dbm(ref_carrier['req_power'])
+    return create_arbitrary_spectral_information(frequency=frequency, signal=signal, baud_rate=baud_rate,
+                                                 slot_width=slot_width, roll_off=roll_off,
+                                                 delta_pdb_per_channel=delta_pdb_per_channel,
+                                                 ref_power=Pref(p_span0=p_span0, p_spani=p_spani))
