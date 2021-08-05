c2df14c587fc52360a49324db74618801bb96852
EstherLerouzic <esther.lerouzic@orange.com>
2021-08-05 15:07:35 +0200

    Add a user defined initial spectrum in propagation functions
    A new function is added to use requests specs based on transceivers
    and on user definition of mixed rates and powers.
    
    Propagation functions are changed so that, if the user defines a
    specific distribution, then they use it else they use as before,
    all identical channels based on the initial request. In this case,
    as before this change, we assume full load, with same channel for
    the spectral info and not the resulting mixt of channels after
    routing.
    
    Signed-off-by: EstherLerouzic <esther.lerouzic@orange.com>
    Change-Id: Icf56396837b77009e98accd27fcebd2dded6d112

--- gnpy/topology/request.py
+++ gnpy/topology/request.py
@@ -23,7 +23,7 @@ from networkx.utils import pairwise
 from numpy import mean, argmin
 from gnpy.core.elements import Transceiver, Roadm
 from gnpy.core.utils import lin2db, dbm2watt
-from gnpy.core.info import create_input_spectral_information
+from gnpy.core.info import create_input_spectral_information, use_initial_spectrum
 from gnpy.core.exceptions import ServiceError, DisjunctionError
 import gnpy.core.ansi_escapes as ansi_escapes
 from copy import deepcopy
@@ -339,10 +339,34 @@ def compute_constrained_path(network, req):
     return total_path
 
 
+def ref_carrier(req_power, equipment):
+    """Create a reference carier based SI information with the specified request's power:
+    ref_carrier['req_power'] records the power in W that the user has defined for a given request
+    (which might be different from the one used for the design).
+    """
+    ref_carrier = {'baud_rate': equipment['SI']['default'].baud_rate}
+    ref_carrier['req_power'] = req_power
+    return ref_carrier
+
+
+def update_spectrum_power(req):
+    """ Initial user defined spectrum may not contain power, in this case use the request's one
+    as default value.
+    """
+    for spectrum in req.initial_spectrum.values():
+        if spectrum['power'] is None:
+            spectrum['power'] = req.power
+
+
 def propagate(path, req, equipment):
-    si = create_input_spectral_information(
-        req.f_min, req.f_max, req.roll_off, req.baud_rate,
-        req.power, req.spacing)
+    """ propagates signals in each element according to initial spectrum set by user
+    """
+    if hasattr(req, 'initial_spectrum'):
+        si = use_initial_spectrum(req.initial_spectrum, ref_carrier=ref_carrier(req.power, equipment))
+    else:
+        si = create_input_spectral_information(
+            req.f_min, req.f_max, req.roll_off, req.baud_rate,
+            req.power, req.spacing)
     for i, el in enumerate(path):
         if isinstance(el, Roadm):
             si = el(si, degree=path[i+1].uid)
@@ -378,9 +402,17 @@ def propagate_and_optimize_mode(path, req, equipment):
             # step2: computes propagation for each baudrate: stop and select the first that passes
             # TODO: the case of roll of is not included: for now use SI one
             # TODO: if the loop in mode optimization does not have a feasible path, then bugs
-            spc_info = create_input_spectral_information(req.f_min, req.f_max,
-                                                         equipment['SI']['default'].roll_off,
-                                                         this_br, req.power, req.spacing)
+            if hasattr(req, 'initial_spectrum'):
+                # add the current explored mode caracteristic on the initial spectrum
+                # maybe use a copy instead of changing the request ?
+                for e in req.initial_spectrum.values():
+                    if e['baud_rate'] is None:
+                        e['baud_rate'] = this_br
+                spc_info = use_initial_spectrum(req.initial_spectrum, ref_carrier=ref_carrier(req.power, equipment))
+            else:
+                spc_info = create_input_spectral_information(req.f_min, req.f_max,
+                                                             equipment['SI']['default'].roll_off,
+                                                             this_br, req.power, req.spacing)
             for i, el in enumerate(path):
                 if isinstance(el, Roadm):
                     spc_info = el(spc_info, degree=path[i+1].uid)
