938e3ada903a9f31cdedd1a7f28236961bf71f67
EstherLerouzic <esther.lerouzic@orange.com>
2021-08-05 17:17:37 +0200

    Add example of spectrum file use in tests
    Signed-off-by: EstherLerouzic <esther.lerouzic@orange.com>
    Change-Id: I030ecff3b0248f986d9d487edf508b9c448515d1

--- tests/test_invocation.py
+++ tests/test_invocation.py
@@ -17,8 +17,14 @@ SRC_ROOT = Path(__file__).parent.parent
     ('openroadm-Stockholm-Gothenburg', transmission_main_example,
      ['-e', 'gnpy/example-data/eqpt_config_openroadm.json', 'gnpy/example-data/Sweden_OpenROADM_example_network.json', ]),
     ('transmission_main_example_long', transmission_main_example,
-     ['-e', 'tests/data/eqpt_config.json', 'tests/data/test_long_network.json'])
-))
+     ['-e', 'tests/data/eqpt_config.json', 'tests/data/test_long_network.json']),
+    ('spectrum1_transmission_main_example', transmission_main_example,
+     ['-spectrum', 'gnpy/example-data/initial_spectrum1.json',  'gnpy/example-data/meshTopologyExampleV2.xls', ]),
+    ('spectrum2_transmission_main_example', transmission_main_example,
+     ['-spectrum', 'gnpy/example-data/initial_spectrum2.json',  'gnpy/example-data/meshTopologyExampleV2.xls', ]),
+    ))
+
+
 def test_example_invocation(capfd, output, handler, args):
     '''Make sure that our examples produce useful output'''
     os.chdir(SRC_ROOT)
