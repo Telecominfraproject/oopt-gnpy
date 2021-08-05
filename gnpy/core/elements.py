54a3725e1788bea722729cc047b04114d5b42f8f
EstherLerouzic <esther.lerouzic@orange.com>
2021-08-05 14:34:13 +0200

    Add a -spectrum option to input external file to define spectrum
    The option is only set for gnpy-transmission-main.
    
    The spectrum file is a list of spectrum objects, each defining
    f_min, f_max and spectrum attributes using the same meaning as SI
    in eqpt_config.json for baud_rate, roll_off, tx_osnr. slot_width is
    used for the occupation of each carrier around their central frequency,
    so slot_width corresponds to spacing of SI.
    Unlike SI, the frequencies are defined includint f_min and f_max.
    The partitions must be contiguous not overlapping.
    
    Pref.p_span0 object records the req_power, while
    ref_carrier records info that will be useful for equalization ie baud_rate.
    
    For now, I have not integrated the possibility to directly use
    transceivers type and mode in the list.
    
    User can define sets of contiguous channels and a label to identify
    the spectrum bands. If no label are defined, the program justs uses
    the index + baud rate of the spectrum bands as label.
    
    Print results per spectrum label
    
    If propagated spectrum has mixed rates, then prints results (GSNR and OSNR)
    for each propagated spectrum type according to its label.
    
    Print per label channel power of elements
    
    Per channel power prints were previously only showing the noiseless
    reference channel power and only an average power.
    With this change, we add a new information on the print:
    the average total power (signal + noise + non-linear noise).
    If there are several spectrum types propagating, the average per
    spectrum is displayed using the label.
    For this purpose, label and total power are recorded in each element
    upon propagation
    
    Note that the difference between this total power and the existing
    channel power represents the added noise for the considered OMS.
    Indeed ROADMs equalize per channel total power, so that power displayed
    in 'actual pch (dBm)' may contain some noise contribution accumulated
    with previous propagation.
    Because 'reference pch out (dBm)' is for the noiseless reference,
    it is exactly set to the target power and 'actual pch (dBm)' is always
    matching 'reference pch out (dBm)' in ROADM prints.
    
    Add examples and tests for -spectrum option
    
    initial_spectrum1.json reproduces exactly the case of SI
    initial_spectrum2.json sets half of the spectrum with 50GHz 32Gbauds and
    half with 75GHz 64 Gbauds. Power setting is not set for the second half,
    So that equalization will depend on ROADM settings.
    
    Signed-off-by: EstherLerouzic <esther.lerouzic@orange.com>
    Change-Id: Ibc01e59e461e5e933e95d23dacbc5289e275ccf7

--- gnpy/core/elements.py
+++ gnpy/core/elements.py
@@ -26,7 +26,8 @@ from scipy.constants import h, c
 from scipy.interpolate import interp1d
 from collections import namedtuple
 
-from gnpy.core.utils import lin2db, db2lin, arrange_frequencies, snr_sum, watt2dbm
+from gnpy.core.utils import lin2db, db2lin, arrange_frequencies, snr_sum, per_label_average, pretty_summary_print, \
+    watt2dbm
 from gnpy.core.parameters import RoadmParams, FusedParams, FiberParams, PumpParams, EdfaParams, EdfaOperational
 from gnpy.core.science_utils import NliSolver, RamanSolver
 from gnpy.core.info import SpectralInformation
@@ -86,6 +87,7 @@ class Transceiver(_Node):
         self.pdl = None
         self.penalties = {}
         self.total_penalty = 0
+        self.propagated_labels = [""]
 
     def _calc_cd(self, spectral_info):
         """ Updates the Transceiver property with the CD of the received channels. CD in ps/nm.
@@ -116,6 +118,7 @@ class Transceiver(_Node):
 
     def _calc_snr(self, spectral_info):
         with errstate(divide='ignore'):
+            self.propagated_labels = spectral_info.label
             self.baud_rate = spectral_info.baud_rate
             ratio_01nm = lin2db(12.5e9 / self.baud_rate)
             # set raw values to record original calculation, before update_snr()
@@ -173,23 +176,22 @@ class Transceiver(_Node):
         if self.snr is None or self.osnr_ase is None:
             return f'{type(self).__name__} {self.uid}'
 
-        snr = round(mean(self.snr), 2)
-        osnr_ase = round(mean(self.osnr_ase), 2)
-        osnr_ase_01nm = round(mean(self.osnr_ase_01nm), 2)
-        snr_01nm = round(mean(self.snr_01nm), 2)
+        snr = per_label_average(self.snr, self.propagated_labels)
+        osnr_ase = per_label_average(self.osnr_ase, self.propagated_labels)
+        osnr_ase_01nm = per_label_average(self.osnr_ase_01nm, self.propagated_labels)
+        snr_01nm = per_label_average(self.snr_01nm, self.propagated_labels)
         cd = mean(self.chromatic_dispersion)
         pmd = mean(self.pmd)
         pdl = mean(self.pdl)
 
         result = '\n'.join([f'{type(self).__name__} {self.uid}',
-
-                          f'  GSNR (0.1nm, dB):          {snr_01nm:.2f}',
-                          f'  GSNR (signal bw, dB):      {snr:.2f}',
-                          f'  OSNR ASE (0.1nm, dB):      {osnr_ase_01nm:.2f}',
-                          f'  OSNR ASE (signal bw, dB):  {osnr_ase:.2f}',
-                          f'  CD (ps/nm):                {cd:.2f}',
-                          f'  PMD (ps):                  {pmd:.2f}',
-                          f'  PDL (dB):                  {pdl:.2f}'])
+                            f'  GSNR (0.1nm, dB):          {pretty_summary_print(snr_01nm)}',
+                            f'  GSNR (signal bw, dB):      {pretty_summary_print(snr)}',
+                            f'  OSNR ASE (0.1nm, dB):      {pretty_summary_print(osnr_ase_01nm)}',
+                            f'  OSNR ASE (signal bw, dB):  {pretty_summary_print(osnr_ase)}',
+                            f'  CD (ps/nm):                {cd:.2f}',
+                            f'  PMD (ps):                  {pmd:.2f}',
+                            f'  PDL (dB):                  {pdl:.2f}'])
 
         cd_penalty = self.penalties.get('chromatic_dispersion')
         if cd_penalty is not None:
@@ -229,6 +231,7 @@ class Roadm(_Node):
         self.passive = True
         self.restrictions = self.params.restrictions
         self.per_degree_pch_out_dbm = self.params.per_degree_pch_out_db
+        self.propagated_labels = [""]
 
     @property
     def to_json(self):
@@ -251,9 +254,11 @@ class Roadm(_Node):
         if self.ref_effective_loss is None:
             return f'{type(self).__name__} {self.uid}'
 
+        total_pch = pretty_summary_print(per_label_average(self.pch_out_dbm, self.propagated_labels))
         return '\n'.join([f'{type(self).__name__} {self.uid}',
-                          f'  effective loss (dB):  {self.ref_effective_loss:.2f}',
-                          f'  pch out (dBm):        {self.ref_pch_out_dbm:.2f}'])
+                          f'  effective loss (dB):     {self.ref_effective_loss:.2f}',
+                          f'  reference pch out (dBm): {self.ref_pch_out_dbm:.2f}',
+                          f'  actual pch out (dBm):    {total_pch}'])
 
     def propagate(self, spectral_info, degree):
         # pin_target and loss are read from eqpt_config.json['Roadm']
@@ -298,6 +303,8 @@ class Roadm(_Node):
         spectral_info.apply_attenuation_db(delta_power)
         spectral_info.pmd = sqrt(spectral_info.pmd ** 2 + self.params.pmd ** 2)
         spectral_info.pdl = sqrt(spectral_info.pdl ** 2 + self.params.pdl ** 2)
+        self.pch_out_dbm = watt2dbm(spectral_info.signal + spectral_info.nli + spectral_info.ase)
+        self.propagated_labels = spectral_info.label
 
     def update_pref(self, spectral_info):
         """Update Reference power
@@ -360,7 +367,7 @@ class Fiber(_Node):
         super().__init__(*args, params=FiberParams(**params), **kwargs)
         self.pch_out_db = None
         self.passive = True
-
+        self.propagated_labels = [""]
         # Raman efficiency matrix function of the delta frequency constructed such that each row is related to a
         # fixed frequency: positive elements represent a gain (from higher frequency) and negative elements represent
         # a loss (to lower frequency)
@@ -410,6 +417,7 @@ class Fiber(_Node):
         if self.pch_out_db is None:
             return f'{type(self).__name__} {self.uid}'
 
+        total_pch = pretty_summary_print(per_label_average(self.pch_out_dbm, self.propagated_labels))
         return '\n'.join([f'{type(self).__name__}          {self.uid}',
                           f'  type_variety:                {self.type_variety}',
                           f'  length (km):                 {self.params.length * 1e-3:.2f}',
@@ -417,7 +425,8 @@ class Fiber(_Node):
                           f'  total loss (dB):             {self.loss:.2f}',
                           f'  (includes conn loss (dB) in: {self.params.con_in:.2f} out: {self.params.con_out:.2f})',
                           f'  (conn loss out includes EOL margin defined in eqpt_config.json)',
-                          f'  pch out (dBm): {self.pch_out_db:.2f}'])
+                          f'  reference pch out (dBm):     {self.pch_out_db:.2f}',
+                          f'  actual pch out (dBm):        {total_pch}'])
 
     def loss_coef_func(self, frequency):
         frequency = asarray(frequency)
@@ -507,6 +516,8 @@ class Fiber(_Node):
         # apply the attenuation due to the output connector loss
         attenuation_out_db = self.params.con_out
         spectral_info.apply_attenuation_db(attenuation_out_db)
+        self.pch_out_dbm = watt2dbm(spectral_info.signal + spectral_info.nli + spectral_info.ase)
+        self.propagated_labels = spectral_info.label
 
     def update_pref(self, spectral_info):
         # in case of Raman, the resulting loss of the fiber is not equivalent to self.loss
@@ -579,6 +590,8 @@ class RamanFiber(Fiber):
         # apply the attenuation due to the output connector loss
         attenuation_out_db = self.params.con_out
         spectral_info.apply_attenuation_db(attenuation_out_db)
+        self.pch_out_dbm = watt2dbm(spectral_info.signal + spectral_info.nli + spectral_info.ase)
+        self.propagated_labels = spectral_info.label
 
 
 class Edfa(_Node):
@@ -607,6 +620,7 @@ class Edfa(_Node):
         self.delta_p = self.operational.delta_p  # delta P with Pref (power swwep) in power mode
         self.tilt_target = self.operational.tilt_target
         self.out_voa = self.operational.out_voa
+        self.propagated_labels = [""]
 
     @property
     def to_json(self):
@@ -640,6 +654,7 @@ class Edfa(_Node):
         if self.pin_db is None or self.pout_db is None:
             return f'{type(self).__name__} {self.uid}'
         nf = mean(self.nf)
+        total_pch = pretty_summary_print(per_label_average(self.pch_out_dbm, self.propagated_labels))
         return '\n'.join([f'{type(self).__name__} {self.uid}',
                           f'  type_variety:           {self.params.type_variety}',
                           f'  effective gain(dB):     {self.effective_gain:.2f}',
@@ -652,6 +667,7 @@ class Edfa(_Node):
                           f'  Delta_P (dB):           ' + (f'{self.delta_p:.2f}' if self.delta_p is not None else 'None'),
                           f'  target pch (dBm):       ' + (f'{self.target_pch_out_db:.2f}' if self.target_pch_out_db is not None else 'None'),
                           f'  effective pch (dBm):    {self.effective_pch_out_db:.2f}',
+                          f'  actual pch out (dBm):   {total_pch}',
                           f'  output VOA (dB):        {self.out_voa:.2f}'])
 
     def interpol_params(self, spectral_info):
@@ -924,6 +940,8 @@ class Edfa(_Node):
         spectral_info.apply_gain_db(self.gprofile - self.out_voa)
         spectral_info.pmd = sqrt(spectral_info.pmd ** 2 + self.params.pmd ** 2)
         spectral_info.pdl = sqrt(spectral_info.pdl ** 2 + self.params.pdl ** 2)
+        self.pch_out_dbm = watt2dbm(spectral_info.signal + spectral_info.nli + spectral_info.ase)
+        self.propagated_labels = spectral_info.label
 
     def update_pref(self, spectral_info):
         spectral_info.pref = \
--- gnpy/core/info.py
+++ gnpy/core/info.py
@@ -56,7 +56,7 @@ class SpectralInformation(object):
 
     def __init__(self, frequency: array, baud_rate: array, slot_width: array, signal: array, nli: array, ase: array,
                  roll_off: array, chromatic_dispersion: array, pmd: array, pdl: array, delta_pdb_per_channel: array,
-                 tx_osnr: array, ref_power: Pref):
+                 tx_osnr: array, ref_power: Pref, label: array):
         indices = argsort(frequency)
         self._frequency = frequency[indices]
         self._df = outer(ones(frequency.shape), frequency) - outer(frequency, ones(frequency.shape))
@@ -84,6 +84,7 @@ class SpectralInformation(object):
         self._delta_pdb_per_channel = delta_pdb_per_channel[indices]
         self._tx_osnr = tx_osnr[indices]
         self._pref = ref_power
+        self._label = label[indices]
 
     @property
     def pref(self):
@@ -159,6 +160,10 @@ class SpectralInformation(object):
     @property
     def pmd(self):
         return self._pmd
+
+    @property
+    def label(self):
+        return self._label
 
     @pmd.setter
     def pmd(self, pmd):
@@ -237,7 +242,8 @@ class SpectralInformation(object):
                                        delta_pdb_per_channel=append(self.delta_pdb_per_channel,
                                                                     other.delta_pdb_per_channel),
                                        tx_osnr=append(self.tx_osnr, other.tx_osnr),
-                                       ref_power=Pref(self.pref.p_span0, self.pref.p_spani, self.pref.ref_carrier))
+                                       ref_power=Pref(self.pref.p_span0, self.pref.p_spani, self.pref.ref_carrier),
+                                       label=append(self.label, other.label))
         except SpectrumError:
             raise SpectrumError('Spectra cannot be summed: channels overlapping.')
 
@@ -263,7 +269,8 @@ def create_arbitrary_spectral_information(frequency: Union[ndarray, Iterable, in
                                           chromatic_dispersion: Union[int, float, ndarray, Iterable] = 0.,
                                           pmd: Union[int, float, ndarray, Iterable] = 0.,
                                           pdl: Union[int, float, ndarray, Iterable] = 0.,
-                                          ref_power: Pref = None):
+                                          ref_power: Pref = None,
+                                          label: Union[str, ndarray, Iterable] = None):
     """This is just a wrapper around the SpectralInformation.__init__() that simplifies the creation of
     a non-uniform spectral information with NLI and ASE powers set to zero."""
     frequency = asarray(frequency)
@@ -281,6 +288,7 @@ def create_arbitrary_spectral_information(frequency: Union[ndarray, Iterable, in
         ase = zeros(number_of_channels)
         delta_pdb_per_channel = full(number_of_channels, delta_pdb_per_channel)
         tx_osnr = full(number_of_channels, tx_osnr)
+        label = full(number_of_channels, label)
         return SpectralInformation(frequency=frequency, slot_width=slot_width,
                                    signal=signal, nli=nli, ase=ase,
                                    baud_rate=baud_rate, roll_off=roll_off,
@@ -288,7 +296,7 @@ def create_arbitrary_spectral_information(frequency: Union[ndarray, Iterable, in
                                    pmd=pmd, pdl=pdl,
                                    delta_pdb_per_channel=delta_pdb_per_channel,
                                    tx_osnr=tx_osnr,
-                                   ref_power=ref_power)
+                                   ref_power=ref_power, label=label)
     except ValueError as e:
         if 'could not broadcast' in str(e):
             raise SpectrumError('Dimension mismatch in input fields.')
@@ -304,11 +312,13 @@ def create_input_spectral_information(f_min, f_max, roll_off, baud_rate, power, 
     p_span0 = watt2dbm(power)
     p_spani = watt2dbm(power)
     delta_pdb_per_channel = zeros(number_of_channels)
+    label = [f'{baud_rate * 1e-9 :.2f}G' for i in range(number_of_channels)]
     return create_arbitrary_spectral_information(frequency, slot_width=spacing, signal=power, baud_rate=baud_rate,
                                                  roll_off=roll_off, delta_pdb_per_channel=delta_pdb_per_channel,
                                                  tx_osnr=tx_osnr,
                                                  ref_power=Pref(p_span0=p_span0, p_spani=p_spani,
-                                                                ref_carrier=ref_carrier))
+                                                                ref_carrier=ref_carrier),
+                                                 label=label)
 
 
 def carriers_to_spectral_information(initial_spectrum: dict[Union[int, float], Carrier], power: float,
@@ -326,13 +336,15 @@ def carriers_to_spectral_information(initial_spectrum: dict[Union[int, float], C
     delta_pdb_per_channel = [c.delta_pdb for c in initial_spectrum.values()]
     slot_width = [c.slot_width for c in initial_spectrum.values()]
     tx_osnr = [c.tx_osnr for c in initial_spectrum.values()]
-    p_span0 = watt2dbm(ref_carrier.req_power)
-    p_spani = watt2dbm(ref_carrier.req_power)
+    label = [c.label for c in initial_spectrum.values()]
+    p_span0 = watt2dbm(power)
+    p_spani = watt2dbm(power)
     return create_arbitrary_spectral_information(frequency=frequency, signal=signal, baud_rate=baud_rate,
                                                  slot_width=slot_width, roll_off=roll_off,
                                                  delta_pdb_per_channel=delta_pdb_per_channel, tx_osnr=tx_osnr,
                                                  ref_power=Pref(p_span0=p_span0, p_spani=p_spani,
-                                                                ref_carrier=ref_carrier))
+                                                                ref_carrier=ref_carrier),
+                                                 label=label)
 
 
 @dataclass
@@ -340,12 +352,14 @@ class Carrier:
     """One channel in the initial mixed-type spectrum definition, each type being defined by
     its delta_pdb (power offset with respect to reference power), baud rate, slot_width, roll_off
     and tx_osnr. delta_pdb offset is applied to target power out of Roadm.
+    Label is used to group carriers which belong to the same partition when printing results.
     """
     delta_pdb: float
     baud_rate: float
     slot_width: float
     roll_off: float
     tx_osnr: float
+    label: str
 
 
 @dataclass
