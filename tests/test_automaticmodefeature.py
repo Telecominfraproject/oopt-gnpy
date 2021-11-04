cb2b3e9df73cd65a13a13acaff9a354721917682
EstherLerouzic <esther.lerouzic@orange.com>
2021-11-04 15:55:07 +0100

    Improve test coverage on request.py
    Co-authored-by: Rodrigo Sasse David <rodrigo.sassedavid@orange.com>
    
    Signed-off-by: EstherLerouzic <esther.lerouzic@orange.com>
    Change-Id: I39d9bc6eac474b7e879e65055081a9c1fe391637

--- tests/test_automaticmodefeature.py
+++ tests/test_automaticmodefeature.py
@@ -17,69 +17,109 @@ from pathlib import Path
 import pytest
 from gnpy.core.network import build_network
 from gnpy.core.utils import automatic_nch, lin2db
-from gnpy.core.elements import Roadm
 from gnpy.topology.request import compute_path_dsjctn, propagate, propagate_and_optimize_mode, correct_json_route_list
 from gnpy.tools.json_io import load_network, load_equipment, requests_from_json, load_requests
 
-network_file_name = Path(__file__).parent.parent / 'tests/data/testTopology_expected.json'
-service_file_name = Path(__file__).parent.parent / 'tests/data/testTopology_testservices.json'
-result_file_name = Path(__file__).parent.parent / 'tests/data/testTopology_testresults.json'
-eqpt_library_name = Path(__file__).parent.parent / 'tests/data/eqpt_config.json'
+
+PARENT_PATH = Path(__file__).parent.parent
+NETWORK_FILE_NAME = PARENT_PATH / 'tests/data/testTopology_expected.json'
+SERVICE_FILE_NAME = PARENT_PATH / 'tests/data/testTopology_testservices.json'
+EQPT_LIBRARY_NAME = PARENT_PATH / 'tests/data/eqpt_config.json'
 
 
-@pytest.mark.parametrize("net", [network_file_name])
-@pytest.mark.parametrize("eqpt", [eqpt_library_name])
-@pytest.mark.parametrize("serv", [service_file_name])
-@pytest.mark.parametrize("expected_mode", [['16QAM', 'PS_SP64_1', 'PS_SP64_1', 'PS_SP64_1', 'mode 2 - fake', 'mode 2', 'PS_SP64_1', 'mode 3', 'PS_SP64_1', 'PS_SP64_1', '16QAM', 'mode 1', 'PS_SP64_1', 'PS_SP64_1', 'mode 1', 'mode 2', 'mode 1', 'mode 2', 'nok']])
-def test_automaticmodefeature(net, eqpt, serv, expected_mode):
-    equipment = load_equipment(eqpt)
-    network = load_network(net, equipment)
-    data = load_requests(serv, eqpt, bidir=False, network=network, network_filename=net)
+@pytest.fixture()
+def test_network():
+    """ Creates simulation set up
+    """
+    equipment = load_equipment(EQPT_LIBRARY_NAME)
+    network = load_network(NETWORK_FILE_NAME, equipment)
+    data = load_requests(SERVICE_FILE_NAME, EQPT_LIBRARY_NAME, bidir=False, network=network,
+                         network_filename=NETWORK_FILE_NAME)
 
     # Build the network once using the default power defined in SI in eqpt config
-    # power density : db2linp(ower_dbm": 0)/power_dbm": 0 * nb channels as defined by
-    # spacing, f_min and f_max
-    p_db = equipment['SI']['default'].power_dbm
+    default = equipment['SI']['default']
+    power_total_db = default.power_dbm + lin2db(automatic_nch(default.f_min,
+                                                              default.f_max,
+                                                              default.spacing))
+    build_network(network, equipment, default.power_dbm, power_total_db)
+    requests = requests_from_json(data, equipment)
+    requests = correct_json_route_list(network, requests)
+    paths = compute_path_dsjctn(network, equipment, requests, disjunctions_list=[])
+    return requests, paths, equipment
 
-    p_total_db = p_db + lin2db(automatic_nch(equipment['SI']['default'].f_min,
-                                             equipment['SI']['default'].f_max, equipment['SI']['default'].spacing))
-    build_network(network, equipment, p_db, p_total_db)
 
-    rqs = requests_from_json(data, equipment)
-    rqs = correct_json_route_list(network, rqs)
-    dsjn = []
-    pths = compute_path_dsjctn(network, equipment, rqs, dsjn)
-    path_res_list = []
+@pytest.mark.parametrize("expected_mode", [['16QAM', 'PS_SP64_1', 'PS_SP64_1', \
+                                            'PS_SP64_1', 'mode 2 - fake', 'mode 2', \
+                                            'PS_SP64_1', 'mode 3', 'PS_SP64_1', \
+                                            'PS_SP64_1', '16QAM', 'mode 1', \
+                                            'PS_SP64_1', 'PS_SP64_1', 'mode 1', \
+                                            'mode 2', 'mode 1', 'mode 2', 'nok']])
+def test_automaticmodefeature(expected_mode, test_network):
+    """ Check that each request produces the expected mode
+    """
+    requests, paths, equipment = test_network
+    path_request_list = []
 
-    for i, pathreq in enumerate(rqs):
-
-        # use the power specified in requests but might be different from the one specified for design
-        # the power is an optional parameter for requests definition
-        # if optional, use the one defines in eqt_config.json
-        p_db = lin2db(pathreq.power * 1e3)
-        p_total_db = p_db + lin2db(pathreq.nb_channel)
-        print(f'request {pathreq.request_id}')
-        print(f'Computing path from {pathreq.source} to {pathreq.destination}')
-        # adding first node to be clearer on the output
-        print(f'with path constraint: {[pathreq.source]+pathreq.nodes_list}')
-
-        total_path = pths[i]
-        print(f'Computed path (roadms):{[e.uid for e in total_path  if isinstance(e, Roadm)]}\n')
-        # for debug
-        # print(f'{pathreq.baud_rate}   {pathreq.power}   {pathreq.spacing}   {pathreq.nb_channel}')
+    for i, pathreq in enumerate(requests):
+        total_path = paths[i]
         if pathreq.baud_rate is not None:
-            print(pathreq.format)
-            path_res_list.append(pathreq.format)
+            path_request_list.append(pathreq.format)
             total_path = propagate(total_path, pathreq, equipment)
         else:
             total_path, mode = propagate_and_optimize_mode(total_path, pathreq, equipment)
             # if no baudrate satisfies spacing, no mode is returned and an empty path is returned
             # a warning is shown in the propagate_and_optimize_mode
             if mode is not None:
-                print(mode['format'])
-                path_res_list.append(mode['format'])
+                path_request_list.append(mode['format'])
             else:
-                print('nok')
-                path_res_list.append('nok')
-    print(path_res_list)
-    assert path_res_list == expected_mode
+                path_request_list.append('nok')
+    assert path_request_list == expected_mode
+
+
+@pytest.mark.parametrize('baud_rate, spacing, min_osnr, expected_mode',
+                         [(39e9, 50e9, 100, 'd'),    # 'd' has highest baud_rate and is feasible
+                          (39e9, 75e9, 100, 'b'),    # 'd' does not fit in spacing, no feasible modes
+                          (32e9, 50e9, 23.5, 'a')])  # 'd' provides more margin, 'a' has a better spectral efficiency
+def test_path_low_osnr(test_network, baud_rate, spacing, min_osnr, expected_mode):
+    """ if no mode work the function returns the last explored mode, else it returns the
+    mode with the highest baudrate. If 2 modes have same rate, it selects the one with the highest
+    bitrate"""
+    requests, paths, equipment = test_network
+    # Condition to achieve path's OSNR lower than mode's
+    path_request = requests[0]
+    total_path = paths[0]
+    modes = [{
+        'format': 'a',
+        'baud_rate': 32e9,
+        'OSNR': min_osnr,
+        'bit_rate': 250e9,
+        'roll_off': 0.15,
+        'tx_osnr': 100,
+        'min_spacing': 50e9,
+        'cost': 1
+        },
+        {
+        'format': 'b',
+        'baud_rate': 32e9,
+        'OSNR': 50,
+        'bit_rate': 100e9,
+        'roll_off': 0.15,
+        'tx_osnr': 100,
+        'min_spacing': 50e9,
+        'cost': 1
+        },
+        {
+        'format': 'd',
+        'baud_rate': baud_rate,
+        'OSNR': 20,
+        'bit_rate': 200e9,
+        'roll_off': 0.15,
+        'tx_osnr': 100,
+        'min_spacing': spacing,
+        'cost': 1
+        }]
+    equipment['Transceiver']['Voyager_16QAM'].mode = modes
+    total_path, mode = propagate_and_optimize_mode(total_path,
+                                                   path_request,
+                                                   equipment)
+    assert mode['format'] == expected_mode
