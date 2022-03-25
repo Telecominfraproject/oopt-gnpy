69df8f646ab7301b247ef6bbd5d156112d4082c2
EstherLerouzic <esther.lerouzic@orange.com>
2022-03-25 09:55:16 +0100

    Change starting frequency for simulations
    Current code starts using user defined f_min + spacing.
    In this commit we change the start to be f_min.
    
    Signed-off-by: EstherLerouzic <esther.lerouzic@orange.com>
    Change-Id: I0c07f547308057071e27c22e6353fe0f153f9983

--- tests/test_science_utils.py
+++ tests/test_science_utils.py
@@ -66,7 +66,7 @@ def test_fiber():
 def test_raman_fiber():
     """ Test the accuracy of propagating the RamanFiber."""
     # spectral information generation
-    spectral_info_input = create_input_spectral_information(f_min=191.3e12, f_max=196.1e12, roll_off=0.15,
+    spectral_info_input = create_input_spectral_information(f_min=191.35e12, f_max=196.15e12, roll_off=0.15,
                                                             baud_rate=32e9, power=1e-3, spacing=50e9)
     SimParams.set_params(load_json(TEST_DIR / 'data' / 'sim_params.json'))
     fiber = RamanFiber(**load_json(TEST_DIR / 'data' / 'test_science_utils_fiber_config.json'))
@@ -103,7 +103,7 @@ def test_fiber_lumped_losses(loss, position, errmsg, set_sim_params):
 def test_fiber_lumped_losses_srs(set_sim_params):
     """ Test the accuracy of Fiber with lumped losses propagation."""
     # spectral information generation
-    spectral_info_input = create_input_spectral_information(f_min=191.3e12, f_max=196.1e12, roll_off=0.15,
+    spectral_info_input = create_input_spectral_information(f_min=191.35e12, f_max=196.15e12, roll_off=0.15,
                                                             baud_rate=32e9, power=1e-3, spacing=50e9)
 
     SimParams.set_params(load_json(TEST_DIR / 'data' / 'sim_params.json'))
