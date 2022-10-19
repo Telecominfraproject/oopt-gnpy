ad55db90d410b29d727037aa134a6782312779b0
EstherLerouzic <esther.lerouzic@orange.com>
2022-10-19 18:27:06 +0200

    Use design delta_p and gains instead of p_spani
    Remove the visualisation of the effective_pch in amp because actual
    and target are the relevant ones. effective_pch was artificially
    related to a mix of reference channel and noisy channel (mixed between
    on the fly redesign but using actual ROADM equalisation which includes noise
    in its actual loss).
    
    the change does no more rely on the target power (which is rounded)
    but on the designed gain, which is not rounded.
    
    Propagations are slightly changed for openroadm simulations because of that.
    (I verified)
    
    The gain of amp was estimated on the fly with p_spni also in case of
    RamanFiber preceding elements. removing p_spani requies that an estimation
    of Raman gain be done during design.
    
    This commit also adds this estimation.
    
    Signed-off-by: EstherLerouzic <esther.lerouzic@orange.com>
    Change-Id: I960b85e99f85a7d168ac5349e325c4928fa5673b

--- gnpy/topology/request.py
+++ gnpy/topology/request.py
@@ -24,6 +24,7 @@ from numpy import mean, argmin
 from gnpy.core.elements import Transceiver, Roadm
 from gnpy.core.utils import lin2db
 from gnpy.core.info import create_input_spectral_information, carriers_to_spectral_information, ReferenceCarrier
+from gnpy.core.network import design_network
 from gnpy.core.exceptions import ServiceError, DisjunctionError
 from copy import deepcopy
 from csv import writer
@@ -1099,6 +1100,7 @@ def compute_path_with_disjunction(network, equipment, pathreqlist, pathlist):
         # elements to simulate performance, several demands having the same destination
         # may use the same transponder for the performance simulation. This is why
         # we use deepcopy: to ensure that each propagation is recorded and not overwritten
+        design_network(pathreq, network, equipment, verbose=False)
         total_path = deepcopy(pathlist[i])
         msg = msg + f'\n\tComputed path (roadms):{[e.uid for e in total_path  if isinstance(e, Roadm)]}'
         LOGGER.info(msg)
