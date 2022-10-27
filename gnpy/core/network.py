cf660a4b953b1f630e9b6685d79c03ce91ec0103
EstherLerouzic <esther.lerouzic@orange.com>
2022-10-27 15:17:37 +0200

    Refactor: remove variety_list variable which is never used
    Probably used from a previous intention to define an set of possible EDFA
    in each element instance and leave GNPy choose the best among this list.
    However the excel/json part of it ha never been implemented.
    
    There is already a way to restrict amplifier choice thanks to
    the "allowed_for_design" flag in each amp. This on applies for all
    amp instances though.
    
    Signed-off-by: EstherLerouzic <esther.lerouzic@orange.com>
    Change-Id: I65c05495a15de1e1a80749e035af5da768fd4b3c

--- gnpy/core/network.py
+++ gnpy/core/network.py
@@ -330,9 +330,7 @@ def set_egress_amplifier(network, this_node, equipment, pref_ch_db, pref_total_d
                     raman_allowed = False
 
                 if node.params.type_variety == '':
-                    if node.variety_list and isinstance(node.variety_list, list):
-                        restrictions = node.variety_list
-                    elif isinstance(prev_node, elements.Roadm) and prev_node.restrictions['booster_variety_list']:
+                    if isinstance(prev_node, elements.Roadm) and prev_node.restrictions['booster_variety_list']:
                         # implementation of restrictions on roadm boosters
                         restrictions = prev_node.restrictions['booster_variety_list']
                     elif isinstance(next_node, elements.Roadm) and next_node.restrictions['preamp_variety_list']:
