b3794cceb12868064d8f6d033d20af0e1d91d0b2
AndreaDAmico <andrea.damico@polito.it>
2023-05-09 15:56:43 +0200

    EDFA Parameters restructuring
    The parameters of the EDFA are explicitely retrieved in the EDFAParams class.
    All the defaults are set instead in the gnpy.tool.json_io.AMP class.
    Where required, the AMP.default_values are used instead of an empty dictionary.
    
    Change-Id: Iba80a6a56bc89feb7e959b54b9bd424ec9b0bf06
    Co-authored-by: Vittorio Gatto <vittoriogatto98@gmail.com>

--- gnpy/core/network.py
+++ gnpy/core/network.py
@@ -9,11 +9,14 @@ Working with networks which consist of network elements
 """
 
 from operator import attrgetter
+from collections import namedtuple
+
 from gnpy.core import ansi_escapes, elements
 from gnpy.core.exceptions import ConfigurationError, NetworkTopologyError
 from gnpy.core.utils import round2float, convert_length
 from gnpy.core.info import ReferenceCarrier
-from collections import namedtuple
+
+from gnpy.tools.json_io import Amp
 
 
 def edfa_nf(gain_target, variety_type, equipment):
@@ -361,7 +364,7 @@ def add_roadm_booster(network, roadm):
         network.remove_edge(roadm, next_node)
         amp = elements.Edfa(
             uid=f'Edfa_booster_{roadm.uid}_to_{next_node.uid}',
-            params={},
+            params=Amp.default_values,
             metadata={
                 'location': {
                     'latitude': roadm.lat,
@@ -387,7 +390,7 @@ def add_roadm_preamp(network, roadm):
         network.remove_edge(prev_node, roadm)
         amp = elements.Edfa(
             uid=f'Edfa_preamp_{roadm.uid}_from_{prev_node.uid}',
-            params={},
+            params=Amp.default_values,
             metadata={
                 'location': {
                     'latitude': roadm.lat,
@@ -416,7 +419,7 @@ def add_inline_amplifier(network, fiber):
         network.remove_edge(fiber, next_node)
         amp = elements.Edfa(
             uid=f'Edfa_{fiber.uid}',
-            params={},
+            params=Amp.default_values,
             metadata={
                 'location': {
                     'latitude': (fiber.lat + next_node.lat) / 2,
