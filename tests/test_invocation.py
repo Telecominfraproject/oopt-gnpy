372dbeccdf93f3da4755dcbadf47c15475c70867
EstherLerouzic <esther.lerouzic@orange.com>
2024-09-05 16:31:13 +0200

    Add an invocation test with multiband case
    Signed-off-by: EstherLerouzic <esther.lerouzic@orange.com>
    Change-Id: I0a98175f0c6b4333fae6bea40dac826032a25233

--- tests/test_invocation.py
+++ tests/test_invocation.py
@@ -36,7 +36,10 @@ SRC_ROOT = Path(__file__).parent.parent
     ('transmission_long_psd', None, transmission_main_example,
      ['-e', 'tests/data/eqpt_config_psd.json', 'tests/data/test_long_network.json', '--spectrum', 'gnpy/example-data/initial_spectrum2.json', ]),
     ('transmission_long_psw', None, transmission_main_example,
-     ['-e', 'tests/data/eqpt_config_psw.json', 'tests/data/test_long_network.json', '--spectrum', 'gnpy/example-data/initial_spectrum2.json', ])
+     ['-e', 'tests/data/eqpt_config_psw.json', 'tests/data/test_long_network.json', '--spectrum', 'gnpy/example-data/initial_spectrum2.json', ]),
+    ('multiband_transmission', None, transmission_main_example,
+     ['gnpy/example-data/multiband_example_network.json', 'Site_A', 'Site_D', '-e', 'gnpy/example-data/eqpt_config_multiband.json',
+      '--spectrum', 'gnpy/example-data/multiband_spectrum.json', '--show-channels'])
 ))
 def test_example_invocation(capfd, caplog, output, log, handler, args):
     """Make sure that our examples produce useful output"""
