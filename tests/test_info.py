ba10f238f9de370d4c8a1bfc83dc3f84872b2de4
gborrach <gborraccini@nec-labs.com>
2026-01-07 05:25:22 -0500

    Add test for spectral information total power method
    Change-Id: I3865b1577e8f58bfdaa4529831f04a6ba7f4bb53

--- tests/test_info.py
+++ tests/test_info.py
@@ -12,10 +12,10 @@ Checks spectral information utilities
 
 import pytest
 from numpy import array, zeros, ones, linspace
-from numpy.testing import assert_array_equal
+from numpy.testing import assert_array_equal, assert_allclose
 from gnpy.core.info import create_arbitrary_spectral_information, create_input_spectral_information
 from gnpy.core.exceptions import SpectrumError
-from gnpy.core.utils import dbm2watt
+from gnpy.core.utils import dbm2watt, watt2dbm
 
 
 def test_create_arbitrary_spectral_information():
@@ -142,3 +142,43 @@ def test_spectral_information_add():
     assert_array_equal(si_c_plus_l.channel_number, si_l_plus_c.channel_number)
     assert_array_equal(si_c_plus_l.number_of_channels, si_l_plus_c.number_of_channels)
     assert_array_equal(si_c_plus_l.df, si_l_plus_c.df)
+
+
+def test_spectral_information_total_power():
+    """
+    Testing Spectral Information total power computation method.
+    """
+
+    tol_lin = 1e-6
+    tol_db = 1e-3
+
+    tx_power = 0.001
+    si = create_input_spectral_information(f_min=191.5e12, f_max=196e12, baud_rate=32e9, spacing=50e9, roll_off=0.1,
+                                           tx_osnr=40.0, tx_power=tx_power)
+
+    # Assuming that there is no noise, pch coincides with signal total power
+    assert_allclose(sum(si.pch), si.ptot, atol=tol_lin, rtol=0.0)
+    assert_allclose(watt2dbm(sum(si.pch)), si.ptot_dbm, atol=tol_db, rtol=0.0)
+    assert_allclose(watt2dbm(sum(si.signal)), si.ptot_dbm, atol=tol_db, rtol=0.0)
+
+    # Injection of ASE increases total power
+    add_ase = tx_power / 100
+    ptot_add_ase = sum(si.pch + add_ase)
+    si.add_ase(add_ase)
+
+    assert_allclose(watt2dbm(sum(si.pch)), si.ptot_dbm, atol=tol_db, rtol=0.0)
+    assert_allclose(watt2dbm(ptot_add_ase), si.ptot_dbm, atol=tol_db, rtol=0.0)
+
+    si_sum_signal_expected = 0.09000000000000007
+    assert_allclose(watt2dbm(sum(si.signal)), watt2dbm(si_sum_signal_expected), atol=tol_db, rtol=0.0)
+
+    # Injection of NLI does not change total power (only power transfer from pch to nli)
+    add_nli = tx_power / 200
+    ptot_before_add_nli_dbm = si.ptot_dbm
+    si.add_nli(add_nli)
+
+    assert_allclose(watt2dbm(sum(si.pch)), si.ptot_dbm, atol=tol_db, rtol=0.0)
+    assert_allclose(ptot_before_add_nli_dbm, si.ptot_dbm, atol=tol_db, rtol=0.0)
+
+    si_sum_signal_expected = 0.08955445544554462
+    assert_allclose(watt2dbm(sum(si.signal)), watt2dbm(si_sum_signal_expected), atol=tol_db, rtol=0.0)
